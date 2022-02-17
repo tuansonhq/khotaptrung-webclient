3ec242929adf3631bc91d4e85329bfd0e81fa2b5
iahman8101 <nhuc69@gmail.com>
2022-02-17 23:23:45 +0700

    fix slide

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -162,14 +162,14 @@
                                         </div>
 
                                     </div>
-                                    <div class="swiper-pagination"></div>
+{{--                                    <div class="swiper-pagination"></div>--}}
                                 </div>
-                                <div class="swiper-button-prev">
-                                    {{--                                    <i class="fas fa-chevron-left"></i>--}}
-                                </div>
-                                <div class="swiper-button-next">
-                                    {{--                                    <i class="fas fa-chevron-right"></i>--}}
-                                </div>
+{{--                                <div class="swiper-button-prev">--}}
+{{--                                    --}}{{--                                    <i class="fas fa-chevron-left"></i>--}}
+{{--                                </div>--}}
+{{--                                <div class="swiper-button-next">--}}
+{{--                                    --}}{{--                                    <i class="fas fa-chevron-right"></i>--}}
+{{--                                </div>--}}
                             </div>
                         </div>
                     </div>
@@ -182,6 +182,215 @@
                         <p style=""><strong>Cực Sốc Siêu Khuyến Mại Tặng 100% Giá Trị Thẻ Nạp Áp Dụng Mọi Mệnh Giá Nhanh Tay Truy Cập Nào</strong>  </p>
 
                     </marquee>
+                </div>
+            </div>
+            <div class="content-items">
+                <div class="container">
+                    <div class="items-title">
+                        <h4>Game Free Fire</h4>
+                        <div class="items-title-lines"></div>
+                    </div>
+                    <div class="game-list row">
+
+                        <div class="col-sm-6 col-lg-3♠ col-md-3 col-6  p-5 ppk">
+                            <div class="game-list-content">
+                                <div class="game-list-image">
+                                    <a href="">
+                                        <img class="game-list-image-sticky" src="https://www.shopas.net/storage/images/pBYgoKE7bt_1621190862.png" alt="">
+                                        <img class="game-list-image-in" src="https://www.shopas.net//storage/images/MyCKUlGT8Q_1642732819.gif" alt="">
+                                    </a>
+                                </div>
+                                <div class="game-list-title">
+                                    <a href="">
+                                        <p><strong>VQ Linh Vật Năm Mới</strong></p>
+
+                                    </a>
+                                </div>
+                                <div class="game-list-description">
+                                    <div class="countime"> </div>
+                                    <p>Đã quay: 388</p>
+                                    <span class="game-list-description-old-price">49,000đ</span>
+                                    <span class="game-list-description-new-price">49,000đ</span>
+                                </div>
+                                <div class="game-list-more">
+                                    <div class="game-list-more-view" >
+                                        <a href="">
+                                            <img src="https://www.shopas.net//storage/images/7Zsng4N5vn_1623839229.gif" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3♠ col-md-3 col-6  p-5 ppk">
+                            <div class="game-list-content">
+                                <div class="game-list-image">
+                                    <a href="">
+                                        <img class="game-list-image-sticky" src="https://www.shopas.net/storage/images/pBYgoKE7bt_1621190862.png" alt="">
+                                        <img class="game-list-image-in" src="https://www.shopas.net//storage/images/MyCKUlGT8Q_1642732819.gif" alt="">
+                                    </a>
+                                </div>
+                                <div class="game-list-title">
+                                    <a href="">
+                                        <p><strong>VQ Linh Vật Năm Mới</strong></p>
+
+                                    </a>
+                                </div>
+                                <div class="game-list-description">
+                                    <div class="countime"> </div>
+                                    <p>Đã quay: 388</p>
+                                    <span class="game-list-description-new-price">49,000đ</span>
+                                </div>
+                                <div class="game-list-more">
+                                    <div class="game-list-more-view" >
+                                        <a href="">
+                                            <img src="https://www.shopas.net//storage/images/7Zsng4N5vn_1623839229.gif" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3♠ col-md-3 col-6  p-5 ppk">
+                            <div class="game-list-content">
+                                <div class="game-list-image">
+                                    <a href="">
+                                        <img class="game-list-image-sticky" src="https://www.shopas.net/storage/images/pBYgoKE7bt_1621190862.png" alt="">
+                                        <img class="game-list-image-in" src="https://www.shopas.net//storage/images/MyCKUlGT8Q_1642732819.gif" alt="">
+                                    </a>
+                                </div>
+                                <div class="game-list-title">
+                                    <a href="">
+                                        <p><strong>VQ Linh Vật Năm Mới</strong></p>
+
+                                    </a>
+                                </div>
+                                <div class="game-list-description">
+                                    <div class="countime"> </div>
+                                    <p>Đã quay: 388</p>
+                                    <span class="game-list-description-new-price">49,000đ</span>
+                                </div>
+                                <div class="game-list-more">
+                                    <div class="game-list-more-view" >
+                                        <a href="">
+                                            <img src="https://www.shopas.net//storage/images/7Zsng4N5vn_1623839229.gif" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3♠ col-md-3 col-6  p-5 ppk">
+                            <div class="game-list-content">
+                                <div class="game-list-image">
+                                    <a href="">
+                                        <img class="game-list-image-sticky" src="https://www.shopas.net/storage/images/pBYgoKE7bt_1621190862.png" alt="">
+                                        <img class="game-list-image-in" src="https://www.shopas.net//storage/images/MyCKUlGT8Q_1642732819.gif" alt="">
+                                    </a>
+                                </div>
+                                <div class="game-list-title">
+                                    <a href="">
+                                        <p><strong>VQ Linh Vật Năm Mới</strong></p>
+
+                                    </a>
+                                </div>
+                                <div class="game-list-description">
+                                    <div class="countime"> </div>
+                                    <p>Đã quay: 388</p>
+                                    <span class="game-list-description-new-price">49,000đ</span>
+                                </div>
+                                <div class="game-list-more">
+                                    <div class="game-list-more-view" >
+                                        <a href="">
+                                            <img src="https://www.shopas.net//storage/images/7Zsng4N5vn_1623839229.gif" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3♠ col-md-3 col-6  p-5 ppk">
+                            <div class="game-list-content">
+                                <div class="game-list-image">
+                                    <a href="">
+                                        <img class="game-list-image-sticky" src="https://www.shopas.net/storage/images/pBYgoKE7bt_1621190862.png" alt="">
+                                        <img class="game-list-image-in" src="https://www.shopas.net//storage/images/MyCKUlGT8Q_1642732819.gif" alt="">
+                                    </a>
+                                </div>
+                                <div class="game-list-title">
+                                    <a href="">
+                                        <p><strong>VQ Linh Vật Năm Mới</strong></p>
+
+                                    </a>
+                                </div>
+                                <div class="game-list-description">
+                                    <div class="countime"> </div>
+                                    <p>Đã quay: 388</p>
+                                    <span class="game-list-description-new-price">49,000đ</span>
+                                </div>
+                                <div class="game-list-more">
+                                    <div class="game-list-more-view" >
+                                        <a href="">
+                                            <img src="https://www.shopas.net//storage/images/7Zsng4N5vn_1623839229.gif" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3♠ col-md-3 col-6  p-5 ppk">
+                            <div class="game-list-content">
+                                <div class="game-list-image">
+                                    <a href="">
+                                        <img class="game-list-image-sticky" src="https://www.shopas.net/storage/images/pBYgoKE7bt_1621190862.png" alt="">
+                                        <img class="game-list-image-in" src="https://www.shopas.net//storage/images/MyCKUlGT8Q_1642732819.gif" alt="">
+                                    </a>
+                                </div>
+                                <div class="game-list-title">
+                                    <a href="">
+                                        <p><strong>VQ Linh Vật Năm Mới</strong></p>
+
+                                    </a>
+                                </div>
+                                <div class="game-list-description">
+                                    <div class="countime"> </div>
+                                    <p>Đã quay: 388</p>
+                                    <span class="game-list-description-new-price">49,000đ</span>
+                                </div>
+                                <div class="game-list-more">
+                                    <div class="game-list-more-view" >
+                                        <a href="">
+                                            <img src="https://www.shopas.net//storage/images/7Zsng4N5vn_1623839229.gif" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3♠ col-md-3 col-6  p-5 ppk">
+                            <div class="game-list-content">
+                                <div class="game-list-image">
+                                    <a href="">
+                                        <img class="game-list-image-sticky" src="https://www.shopas.net/storage/images/pBYgoKE7bt_1621190862.png" alt="">
+                                        <img class="game-list-image-in" src="https://www.shopas.net//storage/images/MyCKUlGT8Q_1642732819.gif" alt="">
+                                    </a>
+                                </div>
+                                <div class="game-list-title">
+                                    <a href="">
+                                        <p><strong>VQ Linh Vật Năm Mới</strong></p>
+
+                                    </a>
+                                </div>
+                                <div class="game-list-description">
+                                    <div class="countime"> </div>
+                                    <p>Đã quay: 388</p>
+                                    <span class="game-list-description-new-price">49,000đ</span>
+                                </div>
+                                <div class="game-list-more">
+                                    <div class="game-list-more-view" >
+                                        <a href="">
+                                            <img src="https://www.shopas.net//storage/images/7Zsng4N5vn_1623839229.gif" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
             <div class="content-items">
