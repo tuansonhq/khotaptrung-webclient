4811d490ab35d769395a8410c765c642b794eba5
iahman8101 <nhuc69@gmail.com>
2022-02-21 17:55:05 +0700

    fix

--- public/assets/frontend/js/action.js
+++ public/assets/frontend/js/action.js
@@ -1,22 +1,21 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    navText: [
-        "<i class='fa fa-caret-left'></i>",
-        "<i class='fa fa-caret-right'></i>"
-    ],
-    autoplay: true,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
+$( document ).ready(function() {
+    $('body').on('scroll',function(){
+        if($(window).width() > 1024){
+            if ($(this).scrollTop() > 100) {
+                $(".nav-bar-container").css("height","90px");
+                $(".nav-bar-category .nav li a").css("line-height","90px");
+                $("header .nav-bar").css("background-color","rgba(0,0,0,0.5)");
+                $(".nav-bar-brand").css("margin","14px");
+
+            } else {
+                $(".nav-bar-container").css("height","120px");
+                $(".nav-bar-category .nav li a").css("line-height","120px");
+                $(".nav-bar-brand").css("margin","20px 0");
+                $("header .nav-bar").css("background-color","rgba(0,0,0,0.8)");
+            }
         }
-    }
-})
+
+
+    });
+
+});
--- resources/views/frontend/layouts/includes/header.blade.php
+++ resources/views/frontend/layouts/includes/header.blade.php
@@ -2,7 +2,7 @@
     <div class="nav-bar">
         <div class="nav-bar-container container">
             <div class="nav-bar-brand">
-                <a href="">
+                <a href="https://shopas.net/">
                     <img src="https://www.shopas.net/storage/images/6BdfImoiWl_1640248137.png" alt="">
                 </a>
 
@@ -26,11 +26,23 @@
             </div>
             <div class="nav-bar-category-mobile">
                     <span>
-                        <a href="/log-in">Đăng nhập</a>
-                        <a href="/regist">Đăng ký</a>
+                        <a href="https://shopas.net/">Đăng nhập</a>
+                        <a href="https://shopas.net/">Đăng ký</a>
                     </span>
-                <button><i class="fas fa-bars"></i></button>
+                <label class="nav-bar-category-mobile-input" for="nav_mobile_input"><i class="fas fa-bars"></i></label>
+                <input type="checkbox" hidden class="name_input" id="nav_mobile_input" >
+                <nav class="nav_mobile">
+                    <ul class="">
+                        <li><a href="https://shopas.net/">Trang chủ</a></li>
+                        <li><a href="">Tặng 100% gt thẻ nè</a></li>
+                        <li><a href="">nạp tiền</a></li>
+                        <li><a href="">tin túc</a></li>
+                        <li><a href="" class="nav_mobile-log-in"><i class="fas fa-user"></i>   Đăng nhập</a></li>
+                        <li><a href="https://shopas.net/" class="nav_mobile-log-in"><i class="fas fa-user"></i>   Đăng ký</a></li>
+                    </ul>
+                </nav>
             </div>
+
         </div>
     </div>
 </header>
