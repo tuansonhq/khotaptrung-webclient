c23618ac7252811db2cda3b59dbfa3a2b0b64b2d
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-03-21 17:28:39 +0700

    Update
    Update

--- config/captcha.php
+++ config/captcha.php
@@ -49,13 +49,62 @@
 //    ]
 //];
 return [
+
+    'characters' => '123456789',
+
     'default'   => [
         'length'    => 3,
+        'width'     => 60,
+        'height'    => 32,
+        'quality'   => 100,
+        'lines'     => -1,
+        'fontSize' => 99,
+        'bgImage'   => false,
+        'bgColor'   => '#ffffff',
+        'fontColors'=> ['#f44336'],
+        'angle'     => 0,
+    ],
+
+    'flat'   => [
+//        'length'    => 3,
+//        'width'     => 60,
+//        'height'    => 32,
+//        'quality'   => 100,
+//        'lines'     => -1,
+//        'bgImage'   => false,
+//        'bgColor'   => '#ffffff',
+//        'fontColors'=> ['#f44336'],
+////        'contrast'  => 5,
+//        'angle'     => 20,
+        'length'    => 3,
+        'width'     => 60,
+        'height'    => 32,
+        'quality'   => 100,
+        'lines'     => -1,
+        'fontSize' => 99,
+        'bgImage'   => false,
+        'bgColor'   => '#ffffff',
+        'fontColors'=> ['#f44336'],
+        'angle'     => 0,
+
+    ],
+
+    'mini'   => [
+        'length'    => 3,
+        'width'     => 60,
+        'height'    => 32,
+    ],
+
+    'inverse'   => [
+        'length'    => 5,
         'width'     => 120,
         'height'    => 36,
-        'quality'   => 10,
-        'math'      => false, //Enable Math Captcha
-//        'expire'    => 60,   //Stateless/API captcha expiration
-    ],
-    // ...
+        'quality'   => 90,
+        'sensitive' => true,
+        'angle'     => 12,
+        'sharpen'   => 10,
+        'blur'      => 2,
+        'invert'    => true,
+        'contrast'  => -5,
+    ]
 ];
