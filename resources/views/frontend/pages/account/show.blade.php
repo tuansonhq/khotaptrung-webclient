b867dc92ccf46302a431baae9cba8faeb7ce9276
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-03-27 09:18:00 +0700

    Update slider
    Update slider

--- resources/views/frontend/pages/account/show.blade.php
+++ resources/views/frontend/pages/account/show.blade.php
@@ -47,10 +47,12 @@
                             </div>
 
                             <div class="swiper-button-prev">
-                                <div class="arrow-left"></div>
+{{--                                <div class="arrow-left"></div>--}}
+                                <i class="fas fa-chevron-left"></i>
                             </div>
                             <div class="swiper-button-next">
-                                <div class="arrow-right"></div>
+                                <i class="fas fa-chevron-right"></i>
+{{--                                <div class="arrow-right"></div>--}}
                             </div>
                         </div>
 
@@ -197,10 +199,19 @@
                                         <div class="col-md-12 pl-0 pr-0 gallery__bottom">
                                             <div class="row">
                                                 <div class="col-md-6">
+                                                    @if(App\Library\AuthCustom::check())
                                                     <a href="/recharge" class="btn btn-success gallery__bottom__span"><i class="fab fa-cc-visa"></i>&ensp;ATM - VÍ ĐIỆN TỬ</a>
+                                                    @else
+                                                    <a href="/login" class="btn btn-success gallery__bottom__span"><i class="fab fa-cc-visa"></i>&ensp;ATM - VÍ ĐIỆN TỬ</a>
+                                                    @endif
                                                 </div>
                                                 <div class="col-md-6">
-                                                    <a href="/nap-the" class="btn btn-success gallery__bottom__span"><i class="fas fa-barcode-alt"></i>&ensp;NẠP THẺ CÀO</a>
+                                                    @if(App\Library\AuthCustom::check())
+                                                        <a href="/nap-the-tu-dong" class="btn btn-success gallery__bottom__span"><i class="fas fa-barcode-alt"></i>&ensp;NẠP THẺ CÀO</a>
+                                                    @else
+                                                        <a href="/login" class="btn btn-success gallery__bottom__span"><i class="fas fa-barcode-alt"></i>&ensp;NẠP THẺ CÀO</a>
+                                                    @endif
+
                                                 </div>
                                             </div>
                                         </div>
@@ -212,229 +223,6 @@
                 </div>
             </div>
 
-            <style>
-                .gallery {
-                    width: 100%;
-                    max-width: 500px;
-                    margin-left: auto;
-                    margin-right: auto;
-                }
-
-                .gallery-slider {
-                    width: 100%;
-                    height: auto;
-                    margin: 0 0 10px 0;
-                }
-
-                .gallery-slider .swiper-slide {
-                    width: 100%;
-                    height: 250px;
-                }
-
-                .gallery-slider .swiper-slide img {
-                    display: block;
-                    width: 100%;
-                    height: 100%;
-                    margin: 0 auto;
-                    object-fit: cover;
-                }
-
-                .gallery-thumbs {
-                    margin-top: 32px;
-                    width: 100%;
-                    padding: 0;
-                    overflow: hidden;
-                }
-
-                .gallery-thumbs .swiper-slide {
-                    width: 50px;
-                    height: 50px;
-                    text-align: center;
-                    overflow: hidden;
-                    opacity: 1;
-                }
-
-                .gallery-thumbs .swiper-slide-active {
-                    opacity: 1;
-                }
-
-                .gallery-thumbs .swiper-slide img {
-                    width: auto;
-                    height: 100%;
-                    object-fit: cover;
-                }
-
-                .gallery .swiper-button-next {
-                    width: 30px;
-                    height: 30px;
-                    border-radius: 50%;
-                    background: #111111;
-                }
-
-                .gallery .swiper-button-next:after {
-                    display: none;
-                }
-
-                .gallery .arrow-right {
-                    display: inline-block;
-                    width: 9px;
-                    height: 9px;
-                    margin: 0 10px;
-                    border-top: 2px solid #fff;
-                    border-right: 2px solid #fff;
-                    transform: rotate(45deg) translate(-3px, 3px);
-                }
-
-                .gallery .swiper-button-prev {
-                    width: 30px;
-                    height: 30px;
-                    border-radius: 50%;
-                    background: #111111;
-                }
-
-                .gallery .swiper-button-prev:after {
-                    display: none;
-                }
-
-                .gallery .arrow-left {
-                    display: inline-block;
-                    width: 9px;
-                    height: 9px;
-                    margin: 0 10px;
-                    border-left: 2px solid #fff;
-                    border-bottom: 2px solid #fff;
-                    transform: rotate(45deg) translate(3px, -3px);
-                }
-            </style>
-
-            <script>
-                $(function(){
-                    var slider = new Swiper ('.gallery-slider', {
-                        autoplay: {
-                            delay: 2000,
-
-                        },
-
-                        slidesPerView: 1,
-                        centeredSlides: true,
-                        loop: true,
-                        loopedSlides: 6, //スライドの枚数と同じ値を指定
-                        navigation: {
-                            nextEl: '.swiper-button-next',
-                            prevEl: '.swiper-button-prev',
-                        },
-                    });
-
-                    var thumbs = new Swiper ('.gallery-thumbs', {
-                        slidesPerView: 5,
-                        spaceBetween: 2, //スライドの枚数と同じ値を指定
-                        centeredSlides: true,
-                        loop: true,
-                        slideToClickedSlide: true,
-                    });
-
-                    slider.controller.control = thumbs;
-                    thumbs.controller.control = slider;
-                });
-            </script>
-
-{{--            <div class="row">--}}
-{{--                <div class="col-12 col-sm-12 col-md-12 col-lg-4">--}}
-{{--                    <div class="shop_product_header">--}}
-{{--                        <p>#{{ $data->id }}</p>--}}
-{{--                        <span>{{ $data_category->title }}̉</span>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-md-12 shop_product_info_mobile">--}}
-{{--                    <a  data-fancybox="gallerycoverDetail" href="https://shopas.net/storage/images/hyi1T9DGM1_1645949761.jpg">--}}
-{{--                        <img src="https://shopas.net/storage/images/hyi1T9DGM1_1645949761.jpg" alt="">--}}
-{{--                    </a>--}}
-{{--                    <div class="shop_product_info">--}}
-{{--                        <div class="shop_product_info_divider">--}}
-{{--                            <i class="fas fa-circle"></i>--}}
-{{--                        </div>--}}
-{{--                        <div class="row">--}}
-
-{{--                            @include('frontend.pages.account.widget.account_load_attribute')--}}
-
-{{--                        </div>--}}
-{{--                        <div class="shop_product_info_divider">--}}
-{{--                            <i class="fas fa-circle"></i>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-12 col-sm-12 col-md-12 col-lg-4">--}}
-{{--                    <div class="shop_product_price">--}}
-{{--                        <div>--}}
-{{--                            {{ formatPrice($data->price_old) }} CARD--}}
-{{--                        </div>--}}
-{{--                        <div>--}}
-{{--                            {{ formatPrice($data->price) }} ATM--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-12 col-sm-12 col-md-12 col-lg-4">--}}
-{{--                    <div class="shop_product_header">--}}
-{{--                        <button type="button" class="mustcard btn c-btn btn-lg c-theme-btn c-font-uppercase c-font-bold c-btn-square m-t-20 buyacc" data-id="{{ $data->id }}">--}}
-{{--                            Mua ngay--}}
-{{--                        </button>--}}
-{{--                        <a href="/recharge" class="btn c-btn btn-lg c-bg-green-4 c-font-white c-font-uppercase c-font-bold c-btn-square m-t-20">ATM - Ví điện tử</a>--}}
-{{--                        <a href="/nap-the" class="btn c-btn btn-lg c-bg-green-4 c-font-white c-font-uppercase c-font-bold c-btn-square m-t-20">Nạp thẻ cào</a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="shop_product_info shop_product_info_desktop">--}}
-{{--                <div class="shop_product_info_divider">--}}
-{{--                    <i class="fas fa-circle"></i>--}}
-{{--                </div>--}}
-{{--                <div class="row">--}}
-{{--                    @if(isset($data->groups))--}}
-{{--                        <?php $att_values = $data->groups ?>--}}
-{{--                        @foreach($att_values as $att_value)--}}
-{{--                            @if($att_value->module == 'acc_label')--}}
-{{--                                <div class="col-md-4 shop_product_info_variant">--}}
-{{--                                    <p>{{ $att_value->parent[0]->title }}: <span>{{ $att_value->title }}</span></p>--}}
-{{--                                </div>--}}
-{{--                            @endif--}}
-{{--                        @endforeach--}}
-{{--                    @endif--}}
-{{--                        <?php $params = json_decode(json_encode($data->params->ext_info),true) ?>--}}
-{{--                    @if(!is_null($dataAttribute) && count($dataAttribute)>0)--}}
-{{--                        @foreach($dataAttribute as $index=>$att)--}}
-{{--                            @if($att->position == 'text')--}}
-{{--                                @if(isset($att->childs))--}}
-{{--                                    @foreach($att->childs as $child)--}}
-
-{{--                                        @foreach($params as $key => $param)--}}
-{{--                                            @if($key == $child->id)--}}
-{{--                                                <div class="col-md-4 shop_product_info_variant">--}}
-{{--                                                    <p>{{ $child->title }}: <span>{{ $param }}</span></p>--}}
-{{--                                                </div>--}}
-{{--                                            @endif--}}
-{{--                                        @endforeach--}}
-{{--                                    @endforeach--}}
-{{--                                @endif--}}
-
-{{--                            @endif--}}
-{{--                        @endforeach--}}
-{{--                    @endif--}}
-{{--                </div>--}}
-{{--                <div class="shop_product_info_divider">--}}
-{{--                    <i class="fas fa-circle"></i>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="shop_product_post">--}}
-{{--                <div class="div">--}}
-{{--                    @foreach(explode('|',$data->image_extension) as $val)--}}
-{{--                        <a data-fancybox="gallerycoverDetail" href="https://media-tt.nick.vn/{{ $val }}">--}}
-{{--                            <img src="https://media-tt.nick.vn/{{ $val }}" alt="">--}}
-{{--                        </a>--}}
-{{--                    @endforeach--}}
-{{--                </div>--}}
-{{--                <div class="pt-3">--}}
-{{--                    <button type="button" class="mustcard btn c-btn btn-lg c-theme-btn c-font-uppercase c-font-bold c-btn-square m-t-20 buyacc" data-id="{{ $data->id }}">Mua ngay</button>--}}
-{{--                </div>--}}
-{{--            </div>--}}
             @if(isset($sliders) && count($sliders))
 
             <div class="shop_product_another pt-3">
@@ -578,6 +366,7 @@
         </script>
     @endif
 
+    <link rel="stylesheet" href="/assets/frontend/css/buyacc.css">
     <script src="/assets/frontend/js/account/buyacc.js"></script>
 
 
