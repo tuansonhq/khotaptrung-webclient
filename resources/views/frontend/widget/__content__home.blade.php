6ef7cab669ff4b0d9fbab363bd1268de2d3124da
vongvd <vongvd@nlbn.net>
2022-03-27 17:18:39 +0700

    add mingigame to home

--- resources/views/frontend/widget/__content__home.blade.php
+++ resources/views/frontend/widget/__content__home.blade.php
@@ -54,3 +54,61 @@
 </div>
 
 @endif
+
+
+@if(isset($dataGame->data) && count($dataGame->data) > 0)
+<div class="content-items" id="freefire_taget">
+    <div class="container">
+        <div class="items-title">
+            <h4>Danh mục minigame</h4>
+            <div class="items-title-lines"></div>
+        </div>
+        <div class="game-list row">
+            @foreach($dataGame->data as $item)
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3  p-5 ppk">
+                    <div class="game-list-content">
+                        <div class="game-list-image">
+                            <a class="account_category" href="/minigame-{{ $item->slug }}">
+                                {{--                                                Anh khuyen mai--}}
+                                @if(isset($item->params->image_percent_sale))
+                                    <img class="game-list-image-sticky" src="{{config('url_medial').$item->params->image_percent_sale}}" alt="">
+                                @else
+                                    <img class="game-list-image-sticky" src="/assets/frontend/images/giamgia.png" alt="">
+                                @endif
+                                @if(isset($item->image))
+                                    <img class="game-list-image-in" src="{{config('url_medial').$item->image }}" alt="">
+                                @else
+                                    <img class="game-list-image-in" src="/assets/frontend/images/ff.jpg" alt="">
+                                @endif
+                                {{--                                                Anh chinh --}}
+
+                            </a>
+                        </div>
+                        <div class="game-list-title">
+                            <a class="account_category" href="/minigame-{{ $item->slug }}">
+                                <p><strong>{{ $item->title }}</strong></p>
+                            </a>
+                        </div>
+                        <div class="game-list-description">
+                            <div class="countime"></div>
+                            <p>Đã quay: {{$item->order_gate_count}} </p>
+                        </div>
+                        <div class="game-list-more">
+                            <div class="game-list-more-view" >
+                                <a class="account_category" href="/minigame-{{ $item->slug }}">
+                                    @if(isset($item->params->image_view_all))
+                                        <img src="{{config('url_medial').$item->params->image_view_all }}" alt="">
+                                    @else
+                                        <img src="/assets/frontend/images/muangay.jpg" alt="">
+                                    @endif
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+
+@endif
