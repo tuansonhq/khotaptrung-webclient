401a77251adaaf95f83468b0caaca9eb1afef545
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-03-28 15:43:56 +0700

    update
    update

--- resources/views/frontend/widget/__seo_head.blade.php
+++ resources/views/frontend/widget/__seo_head.blade.php
@@ -3,5 +3,5 @@
 <meta name="description" content="{{ strip_tags($data->description??setting('sys_description')) }}">
 
 <meta name="keywords" content="{{setting('sys_keyword')}}">
-<link rel="shortcut icon" href="{{config('api.url_media').setting('sys_logo') }}" type="image/x-icon">
+<link rel="shortcut icon" href="{{config('api.url_media').setting('sys_favicon') }}" type="image/x-icon">
 
