377aa595e8eb6c3adefbfefd17085689bce62c74
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-04-12 22:09:48 +0700

    fix theme

--- app/Library/DirectAPITheme.php
+++ app/Library/DirectAPITheme.php
@@ -7,8 +7,9 @@ use Carbon\Carbon;
 class DirectAPITheme
 {
     public static function _makeRequest($url, array $data, $method){
-        $data ['client'] = \Request::server ("HTTP_HOST");
+//        $data ['client'] = \Request::server ("HTTP_HOST");
 //        $data['client'] = config('api.client');
+        $data ['client'] = \Request::server("HTTP_HOST");
         if(is_array($data)){
             $dataPost = http_build_query($data);
         }else{
