4c17cf2b55dc93b2032889797c1788d5b485816f
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-04-15 19:16:42 +0700

    f
    ffffff

--- resources/views/theme_1/frontend/widget/__seo_head.blade.php
+++ resources/views/theme_1/frontend/widget/__seo_head.blade.php
@@ -3,11 +3,11 @@
 
 <meta name="keywords" content="{{setting('sys_keyword')}}">
 <link rel="shortcut icon" href="{{config('api.url_media').setting('sys_favicon') }}" type="image/x-icon">
-
-@if(isset($muathe))
-    <title>{{ $muathe['title'] }}</title>
-    <meta name="description" content="{{ $muathe['description'] }}">
+@if(Request::is('mua-the'))
+    <title>{{setting('sys_store_card_title')??setting('sys_title') }}</title>
+    <meta name="description" content="{{ strip_tags(setting('sys_store_card_seo')??setting('sys_description')) }}">
 @else
     <title>{{$data->title??setting('sys_title') }}</title>
     <meta name="description" content="{{ strip_tags($data->description??setting('sys_description')) }}">
 @endif
+
