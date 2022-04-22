4904b6afedf15b3d59556f93ea490a53abc5ab94
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-04-22 09:57:23 +0700

    fix footer

--- resources/views/theme_2/frontend/layouts/includes/footer.blade.php
+++ resources/views/theme_2/frontend/layouts/includes/footer.blade.php
@@ -48,9 +48,9 @@
 {{--            {!! setting('sys_footer') !!}--}}
 {{--        </div>--}}
     </div>
-    <div class="site-footer-bar">
-        <div class="container">
-            <div class="site-copyright op-5">Copyright © MuatheGarena.com</div>
-        </div>
-    </div>
+{{--    <div class="site-footer-bar">--}}
+{{--        <div class="container">--}}
+{{--            <div class="site-copyright op-5">Copyright © MuatheGarena.com</div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 </div><!-- END Site Footer -->
