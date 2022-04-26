b102c8ea383ae181617bae5ee966f7ca34b84eb5
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-04-26 11:16:55 +0700

    fix meta robots

--- resources/views/theme_1/frontend/layouts/master.blade.php
+++ resources/views/theme_1/frontend/layouts/master.blade.php
@@ -1,10 +1,11 @@
 <!doctype html>
-<html lang="en">
+<html lang="vi">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="index,follow" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="path" content="" />
     <meta name="jwt" content="jwt" />
