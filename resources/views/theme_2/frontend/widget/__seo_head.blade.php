004ba6e0d5e6797b0343827bcda539b649c3773b
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-04-27 15:45:06 +0700

    fix share social

--- resources/views/theme_2/frontend/widget/__seo_head.blade.php
+++ resources/views/theme_2/frontend/widget/__seo_head.blade.php
@@ -5,3 +5,17 @@
 <meta name="keywords" content="{{setting('sys_keyword')}}">
 <link rel="shortcut icon" href="{{config('api.url_media').setting('sys_favicon') }}" type="image/x-icon">
 
+
+
+@if(isset($data->title))
+    <meta property="og:title" content="{{$data->title}}">
+@else
+    <meta property="og:title" content="{{setting('sys_title')}}">
+@endif
+@if(isset($data->image))
+    <meta property="og:image" content="https://media-tt.nick.vn/{{$data->image }}">
+@elseif ( setting('sys_og_image') && setting('sys_og_image') != "")
+    <meta property="og:image" content="{{config('api.url_media').setting('sys_og_image') }}">
+@else
+    <meta property="og:image" content="{{config('api.url_media').setting('sys_logo') }}">
+@endif
