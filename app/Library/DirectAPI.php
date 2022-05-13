29860e735cd541c50b6dc35b926308dd43da9dfb
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-05-13 11:20:52 +0700

    update nạp thẻ

--- app/Library/DirectAPI.php
+++ app/Library/DirectAPI.php
@@ -10,9 +10,10 @@ class DirectAPI{
     public static function _makeRequest($url, array $data, $method){
 
 
-        $data ['domain'] = \Request::server("HTTP_HOST");
-        $data ['client'] = \Request::server("HTTP_HOST");
+        $data ['domain'] = \Request::server ("HTTP_HOST");
+        $data ['client'] =\Request::server ("HTTP_HOST");
 //        $data ['domain'] = config('api.client');
+//        $data ['client'] = config('api.client');
         $data['secret_key'] = config('api.secret_key');
         if(is_array($data)){
             $dataPost = http_build_query($data);
@@ -47,10 +48,12 @@ class DirectAPI{
                 continue;
             }
             else{
-                if($httpcode==401){
-                    session()->flush();
-                }
+//                if($httpcode==401){
+//                    session()->flush();
+//                }
+
                 $result = json_decode($resultRaw);
+
                 $resultChange->response_code = $httpcode;
                 $resultChange->response_data = $result;
                 return $resultChange;
