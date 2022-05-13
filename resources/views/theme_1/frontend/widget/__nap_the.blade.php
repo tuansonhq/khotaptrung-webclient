f2da2d0034fd446736a69af15a1aca17597e15a3
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-05-13 14:23:30 +0700

    update nap thẻ

--- resources/views/theme_1/frontend/widget/__nap_the.blade.php
+++ resources/views/theme_1/frontend/widget/__nap_the.blade.php
@@ -1,60 +1,60 @@
 
-{{--<div class="row justify-content-center" id="loading-data">--}}
-{{--    <div class="loading"></div>--}}
-{{--</div>--}}
-{{--<div class="" id="form-content">--}}
-{{--    <form action="{{route('postTelecomDepositAuto')}}" id="form-charge" method="POST">--}}
-{{--        @csrf--}}
+<div class="row justify-content-center" id="loading-data">
+    <div class="loading"></div>
+</div>
+<div class="" id="form-content">
+    <form action="{{route('postTelecomDepositAuto')}}" id="form-charge" method="POST">
+        @csrf
 
-{{--        <div class="form-group">--}}
-{{--            <div class="col-12">--}}
-{{--                <select name="type"  id="telecom"  class="form-control" >--}}
+        <div class="form-group">
+            <div class="col-12">
+                <select name="type"  id="telecom"  class="form-control" >
 
-{{--                </select>--}}
+                </select>
 
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="form-group">--}}
-{{--            <div class="col-12">--}}
-{{--                <select class="form-control " id="amount" name="amount">--}}
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-12">
+                <select class="form-control " id="amount" name="amount">
 
-{{--                </select>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="form-group">--}}
-{{--            <div class="col-12">--}}
-{{--                <input type="text" placeholder="Mã số thẻ" name="pin" class="form-control" >--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="form-group">--}}
-{{--            <div class="col-12">--}}
-{{--                <input type="text" placeholder="Số serial " name="serial" class="form-control" >--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="form-group row">--}}
-{{--            <div class="col-md-12">--}}
-{{--                <div class="input-group" style="width: 100%">--}}
-{{--                    <input type="text" class="form-control" placeholder="Mã bảo vệ" name="captcha" id="captcha">--}}
-{{--                    <div class="captcha">--}}
-{{--                    <span class="reload"  id="reload">--}}
-{{--                        {!! captcha_img() !!}--}}
-{{--                    </span>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+                </select>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-12">
+                <input type="text" placeholder="Mã số thẻ" name="pin" class="form-control" >
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-12">
+                <input type="text" placeholder="Số serial " name="serial" class="form-control" >
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-12">
+                <div class="input-group" style="width: 100%">
+                    <input type="text" class="form-control" placeholder="Mã bảo vệ" name="captcha" id="captcha">
+                    <div class="captcha">
+                    <span class="reload"  id="reload">
+                        {!! captcha_img() !!}
+                    </span>
+                    </div>
+                </div>
+            </div>
 
-{{--        </div>--}}
-{{--        <div class="form-group" >--}}
-{{--            <div class="col-12">--}}
+        </div>
+        <div class="form-group" >
+            <div class="col-12">
 {{--                @if (App\Library\AuthCustom::check())--}}
-{{--                    <button class="btn btn-submit" type="submit">Nạp thẻ</button>--}}
+                    <button class="btn btn-submit" type="submit">Nạp thẻ</button>
 {{--                @else--}}
 {{--                    <a class="btn btn-submit" onclick="window.location.href='/login'">Nạp thẻ</a>--}}
 
 {{--                @endif--}}
-{{--            </div>--}}
-{{--        </div>--}}
+            </div>
+        </div>
 
-{{--    </form>--}}
-{{--</div>--}}
+    </form>
+</div>
 
