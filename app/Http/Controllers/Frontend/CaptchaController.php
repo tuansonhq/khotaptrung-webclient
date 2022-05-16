c8f28af6c3f30dd7233461cbafef3d266cac56c5
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-05-16 09:30:23 +0700

    update captcha
    test captcha

--- app/Http/Controllers/Frontend/CaptchaController.php
+++ app/Http/Controllers/Frontend/CaptchaController.php
@@ -7,21 +7,15 @@ use Illuminate\Http\Request;
 
 class CaptchaController extends Controller
 {
-    public function index() {
-        return view('index');
+
+    public function index()
+    {
+        return response()->json(['captcha'=> captcha_img('flat')]);
+    }
+    public function reloadCaptcha()
+    {
+
+        return response()->json(['captcha'=> captcha_img('flat')]);
     }
 
-    public function capthcaFormValidate(Request $request) {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email',
-            'username' => 'required',
-            'captcha' => 'required|captcha'
-        ]);
-        dd('thanh cong');
-    }
-
-    public function reloadCaptcha() {
-        return response()->json(['captcha' => captcha_img()]);
-    }
 }
