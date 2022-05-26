2c0ee4b3eb2f32e409cd1bd33bcaafe51c019743
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-05-26 10:24:34 +0700

    Update frontend_route.php

--- routes/frontend_route.php
+++ routes/frontend_route.php
@@ -114,15 +114,8 @@ Route::group(array('middleware' => ['theme']) , function (){
                 Route::get('/dich-vu', [ServiceController::class , "getList"]);
                 Route::get('/dich-vu/{slug}', [ServiceController::class , "getDetail"]);
 
+//                cũ
 
-                Route::get('/mua-acc', [AccController::class , "getCategory"]);
-                Route::get('/mua-acc/{slug}', [AccController::class , "getList"]);
-                Route::get('/related-acc', [AccController::class , "getRelated"]);
-
-                Route::get('/acc/{slug}', [AccController::class , "getDetail"]);
-                Route::get('/acc/{slug}/showacc', [AccController::class , "getShowDetail"]);
-
-                Route::get('/acc/{id}/databuy', [AccController::class , "getBuyAccount"]);
 
 
                 Route::group(['middleware' => ['auth_custom']], function (){
@@ -131,6 +124,15 @@ Route::group(array('middleware' => ['theme']) , function (){
                 });
 
                     Route::group(['middleware' => ['doNotCacheResponse']], function (){
+                        Route::get('/mua-acc', [AccController::class , "getCategory"]);
+                        Route::get('/mua-acc/{slug}', [AccController::class , "getList"]);
+                        Route::get('/related-acc', [AccController::class , "getRelated"]);
+
+                        Route::get('/acc/{slug}', [AccController::class , "getDetail"]);
+                        Route::get('/acc/{slug}/showacc', [AccController::class , "getShowDetail"]);
+
+                        Route::get('/acc/{id}/databuy', [AccController::class , "getBuyAccount"]);
+//                        mới
                         Route::post('/user/account_info', [UserController::class , "getInfo"]);
                         Route::get('/mua-the', [\App\Http\Controllers\Frontend\StoreCardController::class , 'getStoreCard'])->name('getStoreCard');
                         // lấy nhà mạng mua thẻ
