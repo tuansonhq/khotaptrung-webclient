7d9fe976d85f9874c01964c35c455eeb23a8b08a
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-05-30 16:12:10 +0700

    Update captcha.php

--- config/captcha.php
+++ config/captcha.php
@@ -1,52 +1,50 @@
 <?php
 
 return [
-
-    'characters' => '123456789',
-
-    'default'   => [
-        'length'    => 3,
-        'width'     => 60,
-        'height'    => 32,
-        'quality'   => 100,
-        'lines'     => -1,
-        'fontSize' => 99,
-        'bgImage'   => false,
-        'bgColor'   => '#ffffff',
-        'fontColors'=> ['#f44336'],
-        'angle'     => 0,
+    'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
+    'default' => [
+        'length' => 3,
+        'width' => 120,
+        'height' => 36,
+        'quality' => 90,
+        'math' => false,
+        'expire' => 60,
+        'encrypt' => false,
+    ],
+    'math' => [
+        'length' => 3,
+        'width' => 120,
+        'height' => 36,
+        'quality' => 90,
+        'math' => true,
     ],
 
-    'flat'   => [
-        'length'    => 3,
-        'width'     => 60,
-        'height'    => 32,
-        'quality'   => 100,
-        'lines'     => -1,
-        'fontSize' => 99,
-        'bgImage'   => false,
-        'bgColor'   => '#ffffff',
-        'fontColors'=> ['#f44336'],
-        'angle'     => 0,
-
+    'flat' => [
+        'length' => 3,
+        'width' => 160,
+        'height' => 46,
+        'quality' => 90,
+        'lines' => 6,
+        'bgImage' => false,
+        'bgColor' => '#ecf2f4',
+        'fontColors' => ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
+        'contrast' => -5,
     ],
-
-    'mini'   => [
-        'length'    => 3,
-        'width'     => 60,
-        'height'    => 32,
+    'mini' => [
+        'length' => 3,
+        'width' => 60,
+        'height' => 32,
     ],
-
-    'inverse'   => [
-        'length'    => 5,
-        'width'     => 120,
-        'height'    => 36,
-        'quality'   => 90,
+    'inverse' => [
+        'length' => 5,
+        'width' => 120,
+        'height' => 36,
+        'quality' => 90,
         'sensitive' => true,
-        'angle'     => 12,
-        'sharpen'   => 10,
-        'blur'      => 2,
-        'invert'    => true,
-        'contrast'  => -5,
+        'angle' => 12,
+        'sharpen' => 10,
+        'blur' => 2,
+        'invert' => true,
+        'contrast' => -5,
     ]
 ];
