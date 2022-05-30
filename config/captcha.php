b3abb04dc26634ad78707e0a3b5493d52277ed06
iahman8101 <64954568+iahman8101@users.noreply.github.com>
2022-05-30 16:52:10 +0700

    Update captcha.php

--- config/captcha.php
+++ config/captcha.php
@@ -9,7 +9,7 @@ return [
         'quality' => 90,
         'math' => false,
         'expire' => 60,
-        'encrypt' => false,
+        'encrypt' => true,
         'bgColor' => '#fff',
     ],
     'math' => [
