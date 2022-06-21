b325a946f5712e5d9e670c7e2634f0f3de74e3d7
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-06-21 17:27:50 +0700

    aâ
    aaaaaaa

--- routes/frontend_route.php
+++ routes/frontend_route.php
@@ -58,6 +58,7 @@ Route::get('/updategit', function ()
     $command='git pull https://'.config('git.git_secret').'@github.com/tannm2611/khotaptrung-webclient.git '.config('git.git_branch');
 
     $output = shell_exec($command);
+    
     dd($command,$output);
     \Artisan::call('cache:clear');
     \Artisan::call('config:cache');
