9a77ffe5243bfd6fefa0b4be51ed45176a51955b
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-06-21 11:12:54 +0700

    tesstexxe
    tesstexxe

--- routes/frontend_route.php
+++ routes/frontend_route.php
@@ -63,7 +63,7 @@ Route::group(array('middleware' => ['theme']) , function (){
 //                ablcccccccccccccccccccccccc
                 $command='git pull https://'.config('git.git_secret').'@github.com/tannm2611/khotaptrung-webclient.git '.config('git.git_branch');
 
-                $output = shell_exec($command);
+                $output = exec($command);
                 dd($command,$output);
                 \Artisan::call('cache:clear');
                 \Artisan::call('config:cache');
