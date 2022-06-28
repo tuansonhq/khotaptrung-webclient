5973e7362bb89f7978dc0826d171bd0664e2a2c3
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-06-28 21:12:27 +0700

    Update
    Update

---
+++ public/assets/frontend/theme_3/frontend/widget/modal/__rotation_rule.blade.php
@@ -0,0 +1,28 @@
+<div class="modal fade rotation-modal" id="rotationRule" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header rotation-modal-header">
+                <h5 class="modal-title">Thể lệ</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <img src="/assets/frontend/{{theme('')->theme_key}}/image/images_1/close.png" alt="">
+                </button>
+            </div>
+            <div class="modal-body rotation-rule-body">
+                <div class="rotation-rule-img">
+                    <img src="/assets/frontend/{{theme('')->theme_key}}/image/images_1/rotation-rule.png" alt="">
+                </div>
+                <div class="rotation-rule-text">
+                    {!! $thele??null !!}
+                </div>
+                <div class="rotation-modal-btn row no-gutters">
+                    <div class="col-6">
+                        <button class="button-secondary">Chơi thử</button>
+                    </div>
+                    <div class="col-6" style="text-align: right;">
+                        <button class="button-primary">Chơi ngay</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
