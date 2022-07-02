55258ff9453377c46be766c49cb83e35f1f20e03
Duong36 <102272908+Duong36@users.noreply.github.com>
2022-07-02 08:21:34 +0700

    update

--- resources/views/theme_5/frontend/pages/service/list.blade.php
+++ resources/views/theme_5/frontend/pages/service/list.blade.php
@@ -8,7 +8,7 @@
     <section class="media-mobile">
         <div class="container container-fix banner-mobile-container-ct">
             <div class="row marginauto banner-mobile-row-ct">
-                <div class="col-auto left-right" style="width: 10%">
+                <div class="col-auto left-right" style="width: 10%" onclick="openMenuProfile()">
                     <img class="lazy" src="/assets/frontend/{{theme('')->theme_key}}/image/cay-thue/back.png" alt="" >
                 </div>
 
@@ -112,7 +112,7 @@
         </div>
     </section>
 
-    {{--    @include('frontend.pages.service.widget.__category__content')--}}
+        @include('frontend.pages.service.widget.__category__content')
 
     <script src="/assets/frontend/{{theme('')->theme_key}}/js/cay-thue/cay-thue.js?v={{time()}}"></script>
 @endsection
--- resources/views/theme_5/frontend/pages/service/widget/__data__list.blade.php
+++ resources/views/theme_5/frontend/pages/service/widget/__data__list.blade.php
@@ -3,20 +3,21 @@
     <div class="row marginauto body-detail-ct">
         <div class="col-md-12 text-left left-right">
             <div class="row body-detail-row-ct">
-                @foreach($data as $service)
+                @forelse($data as $service)
                     <div class="col-auto body-detail-col-ct js-service">
-                        <a href="/dich-vu/{{@$service->slug }}">
+                        <a href="/dich-vu/{{ @$service->slug }}">
                             <div class="row marginauto hover-overlay-ct">
                                 <div class="col-md-12 default-overlay-ct left-right service--thumbnail">
-                                    <img class="lazy" src="{{@\App\Library\MediaHelpers::media($service->image)}}" alt="{{@$service->slug }}">
+                                    <img class="lazy" src="{{@\App\Library\MediaHelpers::media($service->image)}}" alt="{{ @$service->slug }}">
                                 </div>
                                 <div class="col-md-12 left-right text-center body-detail-col-span-ct">
-                                    <span>{{@$service->title}}</span>
+                                    <span>{{ @$service->title }}</span>
                                 </div>
                             </div>
                         </a>
                     </div>
-                @endforeach
+                @empty
+                @endforelse
             </div>
         </div>
     </div>
