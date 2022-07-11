084e56773f35f91e3bd03a7a5321819e3d7d6549
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-07-11 15:56:03 +0700

    Update css
    Update css

--- resources/views/theme_3/frontend/pages/account/widget/__category__content.blade.php
+++ resources/views/theme_3/frontend/pages/account/widget/__category__content.blade.php
@@ -34,10 +34,10 @@
 
                     <div class="col-md-12 left-right text-center js-toggle-content noselect">
                         <div class="view-more">
-                            <a href="javascript:void(0)" class="global__link__default">Xem thêm<i class="__icon__default --sm__default --link__default ml-1" style="--path : url(/assets/frontend/{{theme('')->theme_key}}/image/icons/arrow-down.png)"></i></a>
+                            <a href="javascript:void(0)" class="global__link__default">Xem thêm<i class="__icon__default --sm__default --link__default ml-1" style="--path : url(/assets/frontend/{{theme('')->theme_key}}/image/svg/xemthem.svg)"></i></a>
                         </div>
                         <div class="view-less">
-                            <a href="javascript:void(0)" class="global__link__default">Thu gọn<i class="__icon__default --sm__default --link__default ml-1" style="--path : url(/assets/frontend/{{theme('')->theme_key}}/image/icons/iconright.png)"></i></a>
+                            <a href="javascript:void(0)" class="global__link__default">Thu gọn<i class="__icon__default --sm__default --link__default ml-1" style="--path : url(/assets/frontend/{{theme('')->theme_key}}/image/svg/rutgon.svg)"></i></a>
                         </div>
                     </div>
 
