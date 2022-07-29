3929310aaa9d0ab39e67b57dd6a5d0fe724e67de
Duong36 <102272908+Duong36@users.noreply.github.com>
2022-07-29 17:30:37 +0700

    update

--- resources/views/theme_5/frontend/pages/service/widget/__related.blade.php
+++ resources/views/theme_5/frontend/pages/service/widget/__related.blade.php
@@ -1,4 +1,4 @@
-@if(isset($datacate))
+
 <section class="section-related-service related-service">
     <div class="section-header c-mb-16 c-mb-lg-8 justify-content-between">
         <h2 class="section-title fz-15 fz-lg-20 lh-lg-24">
@@ -8,43 +8,95 @@
     </div>
     <div class="swiper swiper-related-service">
         <div class="swiper-wrapper">
-            @forelse($datacate as $k_cat => $service)
             <div class="swiper-slide">
                 <div class="card">
                     <div class="card-body c-p-16 scale-thumb">
-                        <a href="/dich-vu/{{@$service->id}}">
+                        <a href="/dich-vu/id">
                             <div class="card-thumb c-mb-8">
-                                <img src="{{@\App\Library\MediaHelpers::media($service->image)}}" alt="" class="card-thumb-image">
+                                <img src="/assets/frontend/{{theme('')->theme_key}}/image/trong/frame1996s8.png" alt="" class="card-thumb-image">
                             </div>
                             <div class="card-attr">
                                 <div class="text-title fw-700">
-                                    {{@$service->title}}
+                                    PUBG Mobile
                                 </div>
                                 <div class="info-attr">
-                                    @if(isset($service->total_order))
-                                        @if($service->params_plus)
-                                            @foreach($service->params_plus as $key => $val)
-                                                @if($key == 'fk_buy')
-                                                    <span>Giao dịch: {{ str_replace(',','.',number_format($service->total_order + $val)) }}</span>
-                                                @endif
-                                            @endforeach
-
-                                        @else
-                                            <span>Giao dịch: {{ str_replace(',','.',number_format($service->total_order)) }}</span>
-                                        @endif
-
-                                    @else
-                                        @if($service->params_plus)
-                                            @foreach($service->params_plus as $key => $val)
-                                                @if($key == 'fk_buy')
-                                                    <span>Giao dịch: {{ str_replace(',','.',number_format($val)) }}</span>
-                                                @endif
-                                            @endforeach
-                                        @else
-                                            <span>Giao dịch: 0</span>
-                                        @endif
-
-                                    @endif
+                                    Đã bán: 45.000
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="card">
+                    <div class="card-body c-p-16 scale-thumb">
+                        <a href="/dich-vu/id">
+                            <div class="card-thumb c-mb-8">
+                                <img src="/assets/frontend/{{theme('')->theme_key}}/image/trong/frame1996s8.png" alt="" class="card-thumb-image">
+                            </div>
+                            <div class="card-attr">
+                                <div class="text-title fw-700">
+                                    PUBG Mobile
+                                </div>
+                                <div class="info-attr">
+                                    Đã bán: 45.000
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="card">
+                    <div class="card-body c-p-16 scale-thumb">
+                        <a href="/dich-vu/id">
+                            <div class="card-thumb c-mb-8">
+                                <img src="/assets/frontend/{{theme('')->theme_key}}/image/trong/frame1996s8.png" alt="" class="card-thumb-image">
+                            </div>
+                            <div class="card-attr">
+                                <div class="text-title fw-700">
+                                    PUBG Mobile
+                                </div>
+                                <div class="info-attr">
+                                    Đã bán: 45.000
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="card">
+                    <div class="card-body c-p-16 scale-thumb">
+                        <a href="/dich-vu/id">
+                            <div class="card-thumb c-mb-8">
+                                <img src="/assets/frontend/{{theme('')->theme_key}}/image/trong/frame1996s8.png" alt="" class="card-thumb-image">
+                            </div>
+                            <div class="card-attr">
+                                <div class="text-title fw-700">
+                                    PUBG Mobile
+                                </div>
+                                <div class="info-attr">
+                                    Đã bán: 45.000
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="card">
+                    <div class="card-body c-p-16 scale-thumb">
+                        <a href="/dich-vu/id">
+                            <div class="card-thumb c-mb-8">
+                                <img src="/assets/frontend/{{theme('')->theme_key}}/image/trong/frame1996s8.png" alt="" class="card-thumb-image">
+                            </div>
+                            <div class="card-attr">
+                                <div class="text-title fw-700">
+                                    PUBG Mobile
+                                </div>
+                                <div class="info-attr">
+                                    Đã bán: 45.000
                                 </div>
                             </div>
                         </a>
@@ -52,9 +104,7 @@
                 </div>
             </div>
         </div>
-        @endforelse
         <div class="navigation slider-next"></div>
         <div class="navigation slider-prev"></div>
     </div>
 </section>
-@endif
