ea862c29ec202f2241b3a8f0d33a24ca288dd973
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-07-31 21:08:08 +0700

    Update seo header
    Update seo header

--- resources/views/theme_5/frontend/pages/account/detail.blade.php
+++ resources/views/theme_5/frontend/pages/account/detail.blade.php
@@ -1,5 +1,10 @@
 @extends('frontend.layouts.master')
-
+@section('seo_head')
+    @include('frontend.widget.__seo_head',with(['data'=>$data]))
+@endsection
+@section('meta_robots')
+    <meta name="robots" content="noindex,noindex" />
+@endsection
 @section('content')
     <div class="container c-container" id="account-detail">
         @if($data == null)
