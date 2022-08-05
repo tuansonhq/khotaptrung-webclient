25da72ae2d6c281184e67e72f22a8055abd0709b
trong44 <trongpv@hqplay.vn>
2022-08-05 11:18:56 +0700

    commit to pull

--- resources/views/theme_5/frontend/pages/storeCard-v2/card-list.blade.php
+++ resources/views/theme_5/frontend/pages/storeCard-v2/card-list.blade.php
@@ -1,6 +1,6 @@
 @extends('frontend.layouts.master')
 @section('styles')
-    <link rel="stylesheet" href="/assets/frontend/{{env('THEME_VERSION')}}/css/trong/buy-card-v2.css">
+    <link rel="stylesheet" href="/assets/frontend/{{theme('')->theme_key}}/css/trong/buy-card-v2.css">
 @endsection
 @section('content')
     <div class="c-container container">
@@ -50,66 +50,7 @@
                             </span>
                         </a>
                         <ul class="collapse card-list show" id="card--game__nav">
-                            <li class="card-item">
-                                <a href="mua-the-garena-v2" class="card-item_link">
-                                    <div class="card-item_thumb mr-2">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png" alt="">
-                                    </div>
-                                    <span class="t-body-1">
-                                        Thẻ Garena
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="card-item">
-                                <a href="mua-the-garena-v2" class="card-item_link">
-                                    <div class="card-item_thumb mr-2">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png" alt="">
-                                    </div>
-                                    <span class="card-item_name">
-                                        Thẻ Gate
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="card-item">
-                                <a href="mua-the-garena-v2" class="card-item_link">
-                                    <div class="card-item_thumb mr-2">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png" alt="">
-                                    </div>
-                                    <span class="card-item_name">
-                                        Thẻ Zing
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="card-item">
-                                <a href="mua-the-garena-v2" class="card-item_link">
-                                    <div class="card-item_thumb mr-2">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png" alt="">
-                                    </div>
-                                    <span class="card-item_name">
-                                        Thẻ Vcoin
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="card-item">
-                                <a href="mua-the-garena-v2" class="card-item_link">
-                                    <div class="card-item_thumb mr-2">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png" alt="">
-                                    </div>
-                                    <span class="card-item_name">
-                                        Thẻ BIT
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="card-item">
-                                <a href="mua-the-garena-v2" class="card-item_link">
-                                    <div class="card-item_thumb mr-2">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png" alt="">
-                                    </div>
-                                    <span class="card-item_name">
-                                        Thẻ Ongame
-                                    </span>
-                                </a>
-                            </li>
+                            <!-- JS PASTE HTML HERE -->
                         </ul>
 
                         <a class="section--card p-lg-3" data-toggle="collapse" href="#card--phone__nav" role="button" aria-expanded="true">
@@ -121,28 +62,7 @@
                             </span>
                         </a>
                         <ul class="collapse card-list show" id="card--phone__nav">
-                            <li class="card-item">
-                                <a href="mua-the-garena-v2" class="card-item_link">
-                                    <div class="card-item_thumb mr-2">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png"
-                                             alt="">
-                                    </div>
-                                    <span class="card-item_name">
-                                        Thẻ Viettel
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="card-item">
-                                <a href="mua-the-garena-v2" class="card-item_link">
-                                    <div class="card-item_thumb mr-2">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png"
-                                             alt="">
-                                    </div>
-                                    <span class="card-item_name">
-                                        Thẻ Mobifone
-                                    </span>
-                                </a>
-                            </li>
+                            <!-- JS PASTE HTML HERE -->
                         </ul>
                     </div>
                 </div>
@@ -156,187 +76,7 @@
                         Thẻ Garena
                     </h2>
                 </div>
-                <div class="row c-mx-n8 list-card d-none d-lg-flex">
-                    <div class="col-lg-3 c-px-8 c-mb-16">
-                        <div class="card">
-                            <div class="card-body c-p-16">
-                                <a href="/mua-the-garena-10k-v2" class="scratch-card c-mb-12">
-                                    <div class="card--thumb">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png"
-                                             class="card--thumb__image py-1 py-lg-0" alt="">
-                                    </div>
-                                    <div class="card--name t-title-2 deno-card-color">
-                                        20.000 VND
-                                    </div>
-                                </a>
-                                <span class="t-sub-2">
-                                    Thẻ Garena 50K
-                                </span>
-                                <div class="t-body-1 link-color">
-                                    Đơn giá: 20.000 đ
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center c-mt-12">
-                                    <span class="t-body-2">
-                                        Số lượng
-                                    </span>
-                                    <div class="js-quantity sm">
-                                        <div class="js-quantity-minus"></div>
-                                        <input type="text" class="js-quantity-input" value="1">
-                                        <div class="js-quantity-add"></div>
-                                    </div>
-                                </div>
-                                <div class="group-btn c-mt-16">
-                                    <a href="" class="btn secondary">
-                                        Chọn mua
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 c-px-8 c-mb-16">
-                        <div class="card">
-                            <div class="card-body c-p-16">
-                                <a href="/mua-the-garena-10k-v2" class="scratch-card c-mb-12">
-                                    <div class="card--thumb">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png"
-                                             class="card--thumb__image py-1 py-lg-0" alt="">
-                                    </div>
-                                    <div class="card--name t-title-2 deno-card-color">
-                                        20.000 VND
-                                    </div>
-                                </a>
-                                <span class="t-sub-2">
-                                    Thẻ Garena 50K
-                                </span>
-                                <div class="t-body-1 link-color">
-                                    Đơn giá: 20.000 đ
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center c-mt-12">
-                                    <span class="t-body-2">
-                                        Số lượng
-                                    </span>
-                                    <div class="js-quantity sm">
-                                        <div class="js-quantity-minus"></div>
-                                        <input type="text" class="js-quantity-input" value="1">
-                                        <div class="js-quantity-add"></div>
-                                    </div>
-                                </div>
-                                <div class="group-btn c-mt-16">
-                                    <a href="" class="btn secondary">
-                                        Chọn mua
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 c-px-8 c-mb-16">
-                        <div class="card">
-                            <div class="card-body c-p-16">
-                                <a href="/mua-the-garena-10k-v2" class="scratch-card c-mb-12">
-                                    <div class="card--thumb">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png"
-                                             class="card--thumb__image py-1 py-lg-0" alt="">
-                                    </div>
-                                    <div class="card--name t-title-2 deno-card-color">
-                                        20.000 VND
-                                    </div>
-                                </a>
-                                <span class="t-sub-2">
-                                    Thẻ Garena 50K
-                                </span>
-                                <div class="t-body-1 link-color">
-                                    Đơn giá: 20.000 đ
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center c-mt-12">
-                                    <span class="t-body-2">
-                                        Số lượng
-                                    </span>
-                                    <div class="js-quantity sm">
-                                        <div class="js-quantity-minus"></div>
-                                        <input type="text" class="js-quantity-input" value="1">
-                                        <div class="js-quantity-add"></div>
-                                    </div>
-                                </div>
-                                <div class="group-btn c-mt-16">
-                                    <a href="" class="btn secondary">
-                                        Chọn mua
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 c-px-8 c-mb-16">
-                        <div class="card">
-                            <div class="card-body c-p-16">
-                                <a href="/mua-the-garena-10k-v2" class="scratch-card c-mb-12">
-                                    <div class="card--thumb">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png"
-                                             class="card--thumb__image py-1 py-lg-0" alt="">
-                                    </div>
-                                    <div class="card--name t-title-2 deno-card-color">
-                                        20.000 VND
-                                    </div>
-                                </a>
-                                <span class="t-sub-2">
-                                    Thẻ Garena 50K
-                                </span>
-                                <div class="t-body-1 link-color">
-                                    Đơn giá: 20.000 đ
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center c-mt-12">
-                                    <span class="t-body-2">
-                                        Số lượng
-                                    </span>
-                                    <div class="js-quantity sm">
-                                        <div class="js-quantity-minus"></div>
-                                        <input type="text" class="js-quantity-input" value="1">
-                                        <div class="js-quantity-add"></div>
-                                    </div>
-                                </div>
-                                <div class="group-btn c-mt-16">
-                                    <a href="" class="btn secondary">
-                                        Chọn mua
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 c-px-8 c-mb-16">
-                        <div class="card">
-                            <div class="card-body c-p-16">
-                                <a href="/mua-the-garena-10k-v2" class="scratch-card c-mb-12">
-                                    <div class="card--thumb">
-                                        <img src="/assets/frontend/theme_5/image/trong/garena.png"
-                                             class="card--thumb__image py-1 py-lg-0" alt="">
-                                    </div>
-                                    <div class="card--name t-title-2 deno-card-color">
-                                        20.000 VND
-                                    </div>
-                                </a>
-                                <span class="t-sub-2">
-                                    Thẻ Garena 50K
-                                </span>
-                                <div class="t-body-1 link-color">
-                                    Đơn giá: 20.000 đ
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center c-mt-12">
-                                    <span class="t-body-2">
-                                        Số lượng
-                                    </span>
-                                    <div class="js-quantity sm">
-                                        <div class="js-quantity-minus"></div>
-                                        <input type="text" class="js-quantity-input" value="1">
-                                        <div class="js-quantity-add"></div>
-                                    </div>
-                                </div>
-                                <div class="group-btn c-mt-16">
-                                    <a href="" class="btn secondary">
-                                        Chọn mua
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="row c-mx-n8 list-card d-none d-lg-flex" id="wrap-desktop">
                     <div class="col-lg-3 c-px-8 c-mb-16">
                         <div class="card">
                             <div class="card-body c-p-16">
@@ -377,7 +117,7 @@
 
                 <div class="d-block d-lg-none">
                     <div class="swiper swiper-card c-my-16">
-                        <div class="swiper-wrapper">
+                        <div class="swiper-wrapper" id="wrap-mobile">
                             <div class="swiper-slide">
                                 <div class="card">
                                     <div class="card-body c-p-16">
@@ -558,7 +298,6 @@
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -619,4 +358,8 @@
                 </div>
         </div>
     </div>
+        <input type="hidden" value="amount" data-key="{{ @$key }}" id="is_view">
+        @endsection
+@section('scripts')
+            <script src="/assets/frontend/{{theme('')->theme_key}}/js/store-card-v2/main.js"></script>
 @endsection
