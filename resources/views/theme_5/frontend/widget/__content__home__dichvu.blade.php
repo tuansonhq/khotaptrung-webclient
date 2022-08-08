955a7cd3cdc7f668dd66358592e36eec4ae7d062
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-08-08 11:03:35 +0700

    Update
    Update

--- resources/views/theme_5/frontend/widget/__content__home__dichvu.blade.php
+++ resources/views/theme_5/frontend/widget/__content__home__dichvu.blade.php
@@ -1,130 +1,71 @@
 @if(isset($data) && count($data) > 0)
-@if(theme('')->theme_config->sys_theme_ver == 'sys_theme_ver3.2')
-<section class="section-related-service c-mb-24 media-web">
-    <div class="section-header c-mb-8 c-mb-lg-16 justify-content-between">
-        <h2 class="section-title fz-lg-15 lh-lg-24">
-            <i class="icon-title c-mr-8" style="--path:url(/assets/frontend/{{theme('')->theme_key}}/image/svg/acc-game.svg)"></i>
-            Dịch vụ game
-        </h2>
-        <a href="/dich-vu" class="link arr-right">Xem tất cả</a>
-    </div>
-    <div class="swiper swiper-related-service">
-        <div class="swiper-wrapper">
-            @foreach($data as $item)
+    <section class="section-related-service c-mt-12 c-mt-lg-6 c-mb-12 c-mb-lg-6 media-web">
+        <div class="section-header c-mb-8 c-mb-lg-16 justify-content-between">
+            <h2 class="section-title fz-lg-15 lh-lg-24">
+                <i class="icon-title c-mr-8" style="--path:url(/assets/frontend/{{theme('')->theme_key}}/image/svg/acc-game.svg)"></i>
+                Dịch vụ game
+            </h2>
+            <a href="/dich-vu" class="link arr-right">Xem tất cả</a>
+        </div>
+        <div class="swiper swiper-related-service">
+            <div class="swiper-wrapper">
+                @foreach($data as $item)
 
-            <div class="swiper-slide">
-                <div class="card">
-                    <div class="card-body c-p-16 c-p-lg-12 scale-thumb">
-                        <a href="/dich-vu/{{ $item->slug}}">
-                            <div class="card-thumb c-mb-8">
-                                <img onerror="imgError(this)" src="{{\App\Library\MediaHelpers::media($item->image)}}" alt="" class="card-thumb-image">
-                            </div>
-                            <div class="card-attr">
-                                <div class="text-title fw-700 text-limit limit-1">
-                                    {{ $item->title  }}
-                                </div>
-                                <div class="info-attr">
-                                    @if(isset($item->total_order))
-                                        @if($item->params_plus)
-                                            @foreach($item->params_plus as $key => $val)
-                                                @if($key == 'fk_buy')
-                                                    Giao dịch: {{ str_replace(',','.',number_format($item->total_order + $val)) }}
+                    <div class="swiper-slide">
+                        <div class="card">
+                            <div class="card-body c-p-16 c-p-lg-12 scale-thumb">
+                                <a href="/dich-vu/{{ $item->slug}}">
+                                    <div class="card-thumb c-mb-8">
+                                        <img onerror="imgError(this)" src="{{\App\Library\MediaHelpers::media($item->image)}}" alt="" class="card-thumb-image">
+                                    </div>
+                                    <div class="card-attr">
+                                        <div class="text-title fw-700 text-limit limit-1">
+                                            {{ $item->title  }}
+                                        </div>
+                                        <div class="info-attr">
+                                            @if(isset($item->total_order))
+                                                @if($item->params_plus)
+                                                    @foreach($item->params_plus as $key => $val)
+                                                        @if($key == 'fk_buy')
+                                                            Giao dịch: {{ str_replace(',','.',number_format($item->total_order + $val)) }}
+                                                        @endif
+                                                    @endforeach
+
+                                                @else
+                                                    Giao dịch: {{ str_replace(',','.',number_format($item->total_order)) }}
                                                 @endif
-                                            @endforeach
 
-                                        @else
-                                            Giao dịch: {{ str_replace(',','.',number_format($item->total_order)) }}
-                                        @endif
-
-                                    @else
-                                        @if($item->params_plus)
-                                            @foreach($item->params_plus as $key => $val)
-                                                @if($key == 'fk_buy')
-                                                    Giao dịch: {{ str_replace(',','.',number_format($val)) }}
+                                            @else
+                                                @if($item->params_plus)
+                                                    @foreach($item->params_plus as $key => $val)
+                                                        @if($key == 'fk_buy')
+                                                            Giao dịch: {{ str_replace(',','.',number_format($val)) }}
+                                                        @endif
+                                                    @endforeach
+                                                @else
+                                                    Giao dịch: 0
                                                 @endif
-                                            @endforeach
-                                        @else
-                                            Giao dịch: 0
-                                        @endif
 
-                                    @endif
-                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
+                        </div>
                     </div>
-                </div>
+
+                @endforeach
             </div>
-
-            @endforeach
+            <div class="navigation slider-next"></div>
+            <div class="navigation slider-prev"></div>
         </div>
-        <div class="navigation slider-next"></div>
-        <div class="navigation slider-prev"></div>
-    </div>
-</section>
+    </section>
 
-<section class="acc-game-v2 media-mobile c-mb-12">
-    <div class="section-header c-mb-24 c-mb-lg-20 justify-content-between">
-        <h2 class="section-title fz-lg-20 lh-lg-24">
-            <i class="icon-title c-mr-8" style="--path:url(/assets/frontend/{{theme('')->theme_key}}/image/svg/acc-game.svg)"></i>
-            Dịch vụ game
-        </h2>
-        <a href="/dich-vu" class="link arr-right">Xem tất cả</a>
-    </div>
-    <div class="list-category">
-        @foreach($data as $item)
-
-        <div class="item-category c-px-8 c-mb-12 c-px-lg-6">
-            <div class="card">
-                <div class="card-body c-p-16 c-p-lg-12 scale-thumb">
-                    <a href="/dich-vu/{{ $item->slug}}">
-                        <div class="card-thumb c-mb-8">
-                            <img onerror="imgError(this)" src="{{\App\Library\MediaHelpers::media($item->image)}}" alt="" class="card-thumb-image">
-                        </div>
-                        <div class="card-attr">
-                            <div class="text-title fw-700">
-                                {{ $item->title  }}
-                            </div>
-                            <div class="info-attr">
-                                @if(isset($item->total_order))
-                                    @if($item->params_plus)
-                                        @foreach($item->params_plus as $key => $val)
-                                            @if($key == 'fk_buy')
-                                                Giao dịch: {{ str_replace(',','.',number_format($item->total_order + $val)) }}
-                                            @endif
-                                        @endforeach
-
-                                    @else
-                                        Giao dịch: {{ str_replace(',','.',number_format($item->total_order)) }}
-                                    @endif
-
-                                @else
-                                    @if($item->params_plus)
-                                        @foreach($item->params_plus as $key => $val)
-                                            @if($key == 'fk_buy')
-                                                Giao dịch: {{ str_replace(',','.',number_format($val)) }}
-                                            @endif
-                                        @endforeach
-                                    @else
-                                        Giao dịch: 0
-                                    @endif
-
-                                @endif
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        @endforeach
-    </div>
-</section>
-@elseif(theme('')->theme_config->sys_theme_ver == 'sys_theme_ver3.0')
-    <section class="acc-game-v2 c-mb-12">
+    <section class="acc-game-v2 media-mobile c-mt-12 c-mt-lg-6 c-mb-12 c-mb-lg-6">
         <div class="section-header c-mb-24 c-mb-lg-20 justify-content-between">
             <h2 class="section-title fz-lg-20 lh-lg-24">
                 <i class="icon-title c-mr-8" style="--path:url(/assets/frontend/{{theme('')->theme_key}}/image/svg/acc-game.svg)"></i>
-                Game có dịch vụ thuê
+                Dịch vụ game
             </h2>
             <a href="/dich-vu" class="link arr-right">Xem tất cả</a>
         </div>
@@ -139,7 +80,7 @@
                                     <img onerror="imgError(this)" src="{{\App\Library\MediaHelpers::media($item->image)}}" alt="" class="card-thumb-image">
                                 </div>
                                 <div class="card-attr">
-                                    <div class="text-title text-limit limit-1 fw-700">
+                                    <div class="text-title fw-700">
                                         {{ $item->title  }}
                                     </div>
                                     <div class="info-attr">
@@ -177,5 +118,4 @@
             @endforeach
         </div>
     </section>
-@endif
 @endif
