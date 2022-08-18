8ef9e68f67f169bedf2b188059469adaafb3b54e
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-08-18 08:51:21 +0700

    Update
    Update

--- resources/views/theme_3/frontend/pages/index.blade.php
+++ resources/views/theme_3/frontend/pages/index.blade.php
@@ -110,7 +110,9 @@
 
 
             @if(theme('')->theme_config->sys_theme_ver == 'sys_theme_ver3.3')
-                @include('frontend.widget.__buy__acc__home')
+{{--                @include('frontend.widget.__buy__acc__home')--}}
+                @include('frontend.widget.__content__home__game__random')
+                @include('frontend.widget.__content__home__game_thuong')
                 {{--                @include('frontend.widget.__log__coin__home')--}}
 
 
