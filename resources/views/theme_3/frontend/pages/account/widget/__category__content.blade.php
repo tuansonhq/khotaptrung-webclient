c0772df7793200a18ffb72723dae213eb73f9cc5
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-09-06 10:34:54 +0700

    Update
    Update

--- resources/views/theme_3/frontend/pages/account/widget/__category__content.blade.php
+++ resources/views/theme_3/frontend/pages/account/widget/__category__content.blade.php
@@ -7,16 +7,20 @@
                         <div class="col-md-12 left-right">
                             <span class="detailViewBlockTitle">Chi tiết dịch vụ</span>
                         </div>
+                        @if(substr($data->custom->content,1200))
                         <div class="col-md-12 left-right footer-row-col-ct content-video-in content-video-in-add detailViewBlockContent">
+                        @else
+                        <div class="col-md-12 left-right footer-row-col-ct content-video-in  detailViewBlockContent">
+                        @endif
                             {!!  $data->custom->content !!}
                         </div>
-
+                        @if(substr($data->custom->content,1200))
                         <div class="col-md-12 left-right text-center js-toggle-content noselect">
                             <div class="view-more">
                                 <a href="javascript:void(0)" class="global__link__default">Xem thêm<i class="__icon__default --sm__default --link__default ml-1" style="--path : url(/assets/frontend/{{theme('')->theme_key}}/image/svg/xemthem.svg)"></i></a>
                             </div>
                         </div>
-
+                        @endif
                     </div>
                 </div>
 
@@ -33,16 +37,20 @@
                             <div class="col-md-12 left-right">
                                 <span class="detailViewBlockTitle">Chi tiết dịch vụ</span>
                             </div>
+                            @if(substr($data->content,1200))
                             <div class="col-md-12 left-right footer-row-col-ct content-video-in content-video-in-add detailViewBlockContent">
+                            @else
+                            <div class="col-md-12 left-right footer-row-col-ct content-video-in  detailViewBlockContent">
+                            @endif
                                 {!!  $data->content !!}
                             </div>
-
+                            @if(substr($data->content,1200))
                             <div class="col-md-12 left-right text-center js-toggle-content noselect">
                                 <div class="view-more">
                                     <a href="javascript:void(0)" class="global__link__default">Xem thêm<i class="__icon__default --sm__default --link__default ml-1" style="--path : url(/assets/frontend/{{theme('')->theme_key}}/image/svg/xemthem.svg)"></i></a>
                                 </div>
                             </div>
-
+                            @endif
                         </div>
                     </div>
 
