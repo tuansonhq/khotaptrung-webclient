32bda1df690b2e54e31e9dd109c9e0898c53db5c
Duong36 <102272908+Duong36@users.noreply.github.com>
2022-09-07 09:27:05 +0700

    update

--- resources/views/theme_3/frontend/layouts/master.blade.php
+++ resources/views/theme_3/frontend/layouts/master.blade.php
@@ -33,7 +33,7 @@
     <link rel="stylesheet" href="/assets/frontend/{{theme('')->theme_key}}/css/css_nam/minigame.css">
     <link rel="stylesheet" href="/assets/frontend/{{theme('')->theme_key}}/css/style_son.css?v={{time()}}">
     <link rel="stylesheet" href="/assets/frontend/{{theme('')->theme_key}}/css/style_trong.css?ver={{time()}}">
-    <link rel="stylesheet" href="/assets/frontend/{{theme('')->theme_key}}/css/style_duong.css">
+    <link rel="stylesheet" href="/assets/frontend/{{theme('')->theme_key}}/css/style_duong.css?v={{time()}}">
     <link rel="stylesheet" href="/assets/frontend/{{theme('')->theme_key}}/css/style_phu/form_element.css">
 
     @if (!\App\Library\AuthCustom::check())
