4f4ddccc614abaab80e8801857a73421010c2e3f
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-09-08 09:42:00 +0700

    â
    a

--- resources/views/theme_1/frontend/pages/article/list.blade.php
+++ resources/views/theme_1/frontend/pages/article/list.blade.php
@@ -67,7 +67,7 @@
                                 @include('frontend.pages.article.widget.__datalist')
                             </div>
                         </div>
-                        @include('frontend.widget.__menu__category__article')
+                        @include('frontend.pages.article.widget.__danh__muc')
                     </div>
                 </div>
             </div>
