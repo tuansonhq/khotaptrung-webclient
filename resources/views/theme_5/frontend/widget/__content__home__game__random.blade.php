f0999db2ddaef350447ae70d37c0f2f9e2cbd281
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-09-13 00:43:04 +0700

    Update
    Update

--- resources/views/theme_5/frontend/widget/__content__home__game__random.blade.php
+++ resources/views/theme_5/frontend/widget/__content__home__game__random.blade.php
@@ -42,17 +42,17 @@
                                         </div>
 
                                         @php
-                                            if (isset($item->params->price_old)) {
-                                                $sale_percent = (($item->params->price_old - $item->params->price) / $item->params->price_old) * 100;
+                                            if (isset($item->price_old)) {
+                                                $sale_percent = (($item->price_old - $item->price) / $item->price_old) * 100;
                                                 $sale_percent = round($sale_percent, 0, PHP_ROUND_HALF_UP);
                                             } else {
                                                 $sale_percent = 0;
                                             }
                                         @endphp
-                                        @if(isset($item->params) && isset($item->params->price))
+                                        @if(isset($item->price))
                                         <div class="price">
-                                            <div class="price-current w-100">{{ str_replace(',','.',number_format($item->params->price)) }} đ</div>
-                                            <div class="price-old c-mr-8">{{ str_replace(',','.',number_format($item->params->price_old??$item->params->price)) }} đ</div>
+                                            <div class="price-current w-100">{{ str_replace(',','.',number_format($item->price)) }} đ</div>
+                                            <div class="price-old c-mr-8">{{ str_replace(',','.',number_format($item->price_old??$item->price)) }} đ</div>
                                             <div class="discount">{{ $sale_percent }}%</div>
                                         </div>
                                         @else
