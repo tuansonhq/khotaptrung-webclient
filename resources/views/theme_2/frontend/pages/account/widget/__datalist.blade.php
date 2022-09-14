609f921eb433d1806b654e939cebdf56f4da220f
trong44 <trongpv@hqplay.vn>
2022-09-14 10:00:20 +0700

    Update Code
    Update Code

--- resources/views/theme_2/frontend/pages/account/widget/__datalist.blade.php
+++ resources/views/theme_2/frontend/pages/account/widget/__datalist.blade.php
@@ -315,7 +315,7 @@
 
 <div class="c-pt-24 w-100">
     @if(isset($items) && count($items))
-        {{ $items->appends(request()->query())->links('pagination::pagination_3_0') }}
+        {{ $items->appends(request()->query())->links('pagination::bootstrap-4') }}
     @endif
 </div>
 
