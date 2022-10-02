3265af150395a67a85d73a794c656c358d5fe154
namdh1111 <106732883+namdh1111@users.noreply.github.com>
2022-10-02 22:47:09 +0700

    change color in theme 1

--- public/assets/frontend/theme_1/js/action.js
+++ public/assets/frontend/theme_1/js/action.js
@@ -3,24 +3,7 @@
 
 $( document ).ready(function() {
 
-    $(document).on('scroll',function(){
-        if($(window).width() > 1024){
-            if ($(this).scrollTop() > 100) {
-                $(".nav-bar-container").css("height","90px");
-                $(".nav-bar-category .nav li a").css("line-height","90px");
-                $("header .nav-bar").css("background-color","rgba(0,0,0,0.5)");
-                $(".nav-bar-brand").css("margin","14px");
 
-
-            } else {
-                $(".nav-bar-container").css("height","120px");
-                $(".nav-bar-category .nav li a").css("line-height","120px");
-                $(".nav-bar-brand").css("margin","20px 0");
-                $("header .nav-bar").css("background-color","rgba(0,0,0,0.8)");
-            }
-        }
-
-    });
     $('.item_play_intro_viewmore').click(function(){
         $('.item_play_intro_viewless').css("display","flex");
         $('.item_play_intro_viewmore').css("display","none");
