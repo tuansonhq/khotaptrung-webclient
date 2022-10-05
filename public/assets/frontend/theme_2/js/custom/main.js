e8c52066ba7dfec504e5651a0f719e28ebf1077a
trong44 <trongpv@hqplay.vn>
2022-10-05 16:42:13 +0700

    Update main.js

--- public/assets/frontend/theme_2/js/custom/main.js
+++ public/assets/frontend/theme_2/js/custom/main.js
@@ -367,5 +367,24 @@ $(document).ready(function() {
         event.preventDefault();
         $(this).closest('.modal').modal('hide');
     })
+
+    $(function() {
+
+        $('.lazy').Lazy({
+            // your configuration goes here
+            placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
+            // scrollDirection: 'vertical',
+            effect: 'fadeIn',
+            visibleOnly: true,
+            afterLoad: function(element) {
+                $('img.lazy').css('background-image','unset')
+            },
+            onFinishedAll: function() {
+                // called once all elements was handled
+            }
+
+        });
+
+    });
 });
 
