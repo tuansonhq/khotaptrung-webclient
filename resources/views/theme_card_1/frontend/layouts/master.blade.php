4ef0fa3ba39b30763a3762e4c37e7964dd9c24bf
iahman8101 <namdh@hqplay.vn>
2022-10-10 22:02:05 +0700

    update the card 1

--- resources/views/theme_card_1/frontend/layouts/master.blade.php
+++ resources/views/theme_card_1/frontend/layouts/master.blade.php
@@ -32,52 +32,7 @@
 <!-- đăng nhập -->
 
 
-<div class="modal fade" id="signin" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-md">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true"><i class="fa fa-times"></i></span></button>
-                <div class="modal-title" id="myModalLabel"><i class="fa fa-sign-in"></i> Đăng nhập</div>
-            </div>
-            <span class="help-block" style="text-align: center;color: #dd4b39;margin-top: 20px;margin-bottom: 0">
-
-                     <strong></strong>
-
-                                    </span>
-
-            <form id="sign_in" role="form" action="https://thegarenagiare.com/login" method="POST">
-                <input type="hidden" name="_token" value="LFU5vc7pZziGJQf9VIxouOMS69I1iKGKLLsACICJ">
-                <div class="panel-body">
-                    <div class="alert alert-danger" id="divnotify"><i class="fa fa-info-circle fa-lg"></i><span></span></div>
-                    <div class="form-group">
-                        <label for="ctrlusername">Tài khoản</label>
-                        <input type="text" name="username" id="ctrlusername" class="form-control" placeholder="Tài khoản đăng nhập" tabindex="1"   autofocus="autofocus" />
-                    </div>
-                    <div class="form-group">
-                        <label for="ctrlpass">Mật khẩu:</label>
-                        <input type="password" name="password" id="ctrlpass" class="form-control" autocomplete="off" placeholder="Mật khẩu" tabindex="2" />
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary" id="ctrlloginbtn" tabindex="3"><i class="fa fa-sign-in"></i> Đăng nhập</button>
-                        &nbsp;
-                    </div>
-                    <div class="text-form text-center">
-                        <p>----  Hoặc  ----</p>
-                    </div>
-                    <div class="form-group m-form__group text-center">
-                        <a style="" href="http://fb.nhapnick.com/thegarenagiare_com" class=""><i class="fab fa-facebook-square" style="font-size: 33px"></i></a>
-                    </div>
-
-                    <!-- <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#forgotyourpassword">Quên mật khẩu?</a> -->
-                </div>
-                <div class="panel-footer"><a class="pull-right" href="https://thegarenagiare.com/register"><i class="fa fa-share-square-o"></i> Bạn chưa có tài khoản? Đăng ký ngay</a>
-                    <div class="clearfix"></div>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
+@include('frontend.widget.modal.__login')
 
 <!-- quên mật khẩu -->
 <div class="modal fade" id="forgotyourpassword" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -131,224 +86,9 @@
 
 <div id="main_home">
     <div class="container">
-
-
-        <style>
-            .banner_index img{margin: auto;
-                text-align: center;
-                display: block;}
-            .card_process {
-                margin-top: -23px;
-            }
-
-            #tab-supplier .nav-link.link-supplier {
-                color: #6f727d;
-                padding: 0px !important;
-            }
-
-            #tab-supplier label {
-                border: 1px solid #E3E3E3;
-                padding: 5px;
-                display: block;
-                position: relative;
-                margin: 0px 14px 14px 14px;
-                cursor: pointer;
-                width: 135px;
-                height: 75px;
-                border-radius: 4px;
-            }
-
-            #tab-supplier input[type="radio"][id^="myCheckbox"] {
-                display: none;
-            }
-
-            #tab-supplier {
-                display: -webkit-box;
-                display: -ms-flexbox;
-                display: flex;
-                -ms-flex-wrap: wrap;
-                flex-wrap: wrap;
-                padding-left: 0;
-                margin-bottom: 0;
-                list-style: none;
-                padding-top: 20px;
-            }
-
-
-            label.item-wapper img {
-                max-width: 100% !important;
-                max-height: 100% !important;
-            }
-
-            #tab-supplier :checked + label {
-                border: 1px solid #F25922;
-            }
-
-            #tab-supplier :checked + label::before {
-                content: "";
-                background: url('https://thegarenagiare.com/assets/frontend/images/new_index/checked_card.svg') no-repeat;
-                background-size: 100% 100%;
-                width: 23px;
-                height: 23px;
-                position: absolute;
-                top: -11px;
-                right: -10px;
-                z-index: 1;
-            }
-
-            .tbl_card_discount tbody tr td:first-child {
-                width: 50%;
-            }
-            .wapper-content {
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: center;
-            }
-
-            .text-center {
-                text-align: center !important;
-            }
-
-            #render-supplier label {
-                width: 135px;
-                height: 65px;
-                border-radius: 4px;
-                /* border: 1px solid #2F6A7C; */
-                margin: 10px;
-                cursor: pointer;
-            }
-
-            .wapper-content .item-content {
-                cursor: pointer !important;
-                width: 135px;
-                height: 65px;
-                border-radius: 4px;
-                /* margin-right: 10px; */
-                margin: 0px 5px;
-                border: 1px solid #2F6A7C;
-                margin-bottom: 10px;
-            }
-
-            #render-supplier input[type="radio"][id^="CheckboxSupplier"] {
-                display: none;
-            }
-
-            .wapper-content .item-content h5 {
-                margin-top: 10px;
-                font-weight: 700;
-                font-size: 16px;
-                line-height: 19px;
-                letter-spacing: .01em;
-                color: #000;
-                text-align: center;
-            }
-
-            .wapper-content .item-content p {
-                font-size: 11px;
-                font-weight: bold;
-                color: #2F6A7C;
-            }
-
-            .wapper-content .item-content span {
-                color: #F25922;
-            }
-
-            #render-supplier label.item-content {
-                position: relative;
-                border: 1px solid #E3E3E3;
-            }
-
-            #render-supplier label.item-content.active {
-                border: 1px solid #F25922;
-            }
-
-            #render-supplier label.item-content.active::before {
-                content: "";
-                background: url("https://thegarenagiare.com/assets/frontend/images/new_index/checked_card.svg") no-repeat;
-                background-size: 100% 100%;
-                width: 23px;
-                height: 23px;
-                position: absolute;
-                top: -11px;
-                right: -10px;
-                z-index: 1;
-            }
-
-            #quantity {
-                height: 40px;
-                width: 120px;
-            }
-
-            .info-payment td {
-                text-align: left !important;
-                padding-left: 25px !important;
-            }
-
-            .wapper-pay button {
-                width: 100%;
-                margin: 15px 0px;
-                font-size: 16px;
-                color: #fff;
-                background: #f25922;
-                font-weight: 600;
-                text-transform: uppercase;
-                height: 40px;
-
-            }
-
-            .wapper-pay button:hover {
-                color: #fff;
-                background: #d87957;
-            }
-
-            .wapper-pay button, .wapper-pay button:visited, .wapper-pay button:focus {
-                outline: 0 !important;
-                color: #ffffff !important;
-            }
-
-
-            @media (max-width: 991.98px) {
-
-
-                .content-supplier {
-                    overflow-x: scroll;
-                    flex-wrap: wrap;
-
-                }
-
-                #supplier-flex {
-                    width: 900px;
-                    padding-top: 20px;
-                }
-
-                #tab-supplier label {
-                    border: 1px solid #E3E3E3;
-                    padding: 5px;
-                    display: block;
-                    position: relative;
-                    margin: 1px 4px 14px 0px;
-                    cursor: pointer;
-                    width: 135px;
-                    height: 75px;
-                    border-radius: 4px;
-                }
-
-                #tab-supplier li:first-child label {
-                    margin: 1px 4px 14px 1px !important;
-                }
-
-
-            }
-
-
-        </style>
         @yield('content')
-
-
         <script>
-
             function reply_click(clicked_id) {
-
                 let html = '';
                 var getamount = $.get("/mua-the/get-amount?telecom_key=" + clicked_id, function (data, status) {
                     $.each(data.store_telecom_value, function (key, value) {
@@ -381,8 +121,6 @@
                     $(".render_quantity").html("1");
                     $(".total_price").html("0");
                 });
-
-
                 // var pos1 = $('#content');
                 // if (pos1.length) {
                 //     var top = pos1.offset().top;
@@ -391,15 +129,10 @@
                 //         scrollTop: top
                 //     }, 500);
                 // }
-
-
-
             }
 
             function formatNumber(num) {
-
                 var num = num+"";
-
                 if(num!="undefined"){
                     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
                 }
@@ -409,7 +142,6 @@
             }
 
             $(document).ready(function () {
-
                 $.ajax({
                     url: "/mua-the",
                     type: "GET",
@@ -499,11 +231,6 @@
 
             $("#recharge_supplier").on('submit',function (event) {
                 event.preventDefault();
-
-
-
-
-
                 var formData = new FormData(this);
                 $.ajax({
                     type: 'POST',
@@ -606,8 +333,6 @@
 </div>
 @include('frontend.layouts.includes.footer')
 
-
-
 <div class="modal fade" id="LoadModal" role="dialog" style="display: none;" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="loader" style="text-align: center"><img src="/assets/frontend/images/loader.gif" style="width: 50px;height: 50px;display: none"></div>
@@ -617,114 +342,17 @@
 </div>
 
 
-
-
-<script>
-    $(document).ready(function () {
-        $('.load-modal').each(function (index, elem) {
-            $(elem).unbind().click(function (e) {
-                e.preventDefault();
-                var curModal = $('#LoadModal');
-                curModal.find('.modal-content').html("<div class=\"loader\" style=\"text-align: center\"><i class=\"fas fa-spinner fa-spin\" style='font-size: 30px'></i></div>");
-                curModal.modal('show').find('.modal-content').load($(elem).attr('rel'));
-            });
-        });
-    });
-</script>
-
-
-
-<style type="text/css">
-    .modal{
-        top: 100px;
-    }
-    #call_icon {
-        position: fixed;
-        right: 10px;
-        bottom: 80px;
-        z-index: 9999999;
-        display: none;
-    }
-
-    @media  screen and (max-width: 480px) {
-        #call_icon {
-            display: block;
-        }
-    }
-</style>
-
-<script>
-    $(function () {
-        var url = window.location.href;
-        $("#navbar-main li").removeClass("active");
-
-        $("#navbar-main a").each(function () {
-
-
-            if (url == (this.href)) {
-                $(this).closest("a").addClass("active");
-                $(this).closest("li").parents('li').addClass("active");
-            }
-        });
-    });
-
-</script>
-
 <div id="m_scroll_top" class="m-scroll-top">
     <i class="la la-arrow-up"></i>
 </div>
 
-<script>
-    $(document).ready(function () {
-        $(".shownewx").click(function () {
-            $(".showmore").css("right", "-27px");
-            $(".bg_gra").fadeIn("slow");
-        });
-        $(".close_nav").click(function () {
-            $(".showmore").css("right", "-110vw");
-            $(".bg_gra").fadeOut("slow");
-        });
-        $(".menu li.m1").hover(function () {
-            $(this).find(".mini_menu").addClass("show");
-        }, function () {
-            $(this).find(".mini_menu").removeClass("show");
-        });
-    });
-</script>
-
-
 <script src="/assets/frontend/{{theme('')->theme_key}}/lib/lazysizes.min.js"></script>
 <script src="/assets/frontend/{{theme('')->theme_key}}/lib/sweetalert/sweetalert.min.js"></script>
+<script src="/assets/frontend/{{theme('')->theme_key}}/js/main.js"></script>
 
 <button type="button" class="btn btn-danger btn-floating btn-lg ripple-surface" id="btn-back-to-top" style="display: none; min-width: 45px;z-index: 999">
     <i class="fas fa-arrow-up"></i>
 </button>
-<script>
-    $(window).scroll(function() {
-        if ($(this).scrollTop()) {
-            $('.c-layout-header-fixed').removeClass('fixtop');
-            $('.c-layout-header-fixed').addClass('fixscroll');
-            $("#btn-back-to-top").show();
-        } else {
-            $('.c-layout-header-fixed').removeClass('fixscroll');
-            $('.c-layout-header-fixed').addClass('fixtop');
-            $("#btn-back-to-top").hide();
-        }
-    });
-    $("#btn-back-to-top").click(function(){
-        document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
-    });
-</script>
-<style>
-    .fixscroll header{position: fixed;
-        width: 100%;
-        z-index: 9999;}
-    #btn-back-to-top{display: block;
-        min-width: 45px;
-        position: fixed;
-        bottom: 100px;
-        right: 30px;}
-</style>
 
 
 </body>
