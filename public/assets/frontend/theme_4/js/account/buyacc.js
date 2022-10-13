e7646033ce7cb8166f6da8d576dee6bb3d8bbdb6
TienPhu0112 <phunt@hqplay.vn>
2022-10-13 15:32:44 +0700

    nick auto lol theme_4

--- public/assets/frontend/theme_4/js/account/buyacc.js
+++ public/assets/frontend/theme_4/js/account/buyacc.js
@@ -1,5 +1,72 @@
 $(document).ready(function () {
 
+    function convertToSlug(title) {
+        var slug;
+        //Đổi chữ hoa thành chữ thường
+        slug = title.toLowerCase();
+        //Đổi ký tự có dấu thành không dấu
+        slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a');
+        slug = slug.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e');
+        slug = slug.replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i');
+        slug = slug.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o');
+        slug = slug.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u');
+        slug = slug.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y');
+        slug = slug.replace(/đ/gi, 'd');
+        //Xóa các ký tự đặt biệt
+        slug = slug.replace(/\`|\~|\!|\@|\#|\||\$|\%|\^|\&|\*|\(|\)|\+|\=|\,|\.|\/|\?|\<|\'|\"|\:|\;|_/gi, '');
+        //Đổi khoảng trắng thành ký tự gạch ngang
+        slug = slug.replace(/ /gi, "-");
+        //Đổi nhiều ký tự gạch ngang liên tiếp thành 1 ký tự gạch ngang
+        //Phòng trường hợp người nhập vào quá nhiều ký tự trắng
+        slug = slug.replace(/\-\-\-\-\-/gi, '-');
+        slug = slug.replace(/\-\-\-\-/gi, '-');
+        slug = slug.replace(/\-\-\-/gi, '-');
+        slug = slug.replace(/\-\-/gi, '-');
+        //Xóa các ký tự gạch ngang ở đầu và cuối
+        slug = '@' + slug + '@';
+        slug = slug.replace(/\@\-|\-\@|\@/gi, '');
+        // trả về kết quả
+        return slug;
+    }
+
+    $('body').on('click','.submit--search',function(e){
+        e.preventDefault();
+        let recentModal = $(this).closest('.modal-nick-auto-lol');
+        let keyword = convertToSlug($(recentModal).find('.keyword--search').val());
+
+        let index = 0;
+        let listItem = recentModal.find('.item-nick-lmht');
+        $(listItem).each(function (i,elm) {
+            $(recentModal).find('.body-modal__nick__text-error').css('display','none');
+            let slug_item = $(elm).find('img').attr('alt');
+            slug_item = convertToSlug(slug_item);
+            $(this).toggle(slug_item.indexOf(keyword) > -1);
+            if (slug_item.indexOf(keyword) > -1){
+                index = index + 1
+            }else {}
+
+            if (index == 0){
+                $(recentModal).find('.body-modal__nick__text-error').css('display','block');
+            }
+
+        })
+    })
+
+    $(document).on('click', '.lm_xemthem_tuong', function (e) {
+        e.preventDefault();
+        $('#modal-champion-list').modal('show');
+    });
+
+    $(document).on('click', '.lm_xemthem_linhthu', function (e) {
+        e.preventDefault();
+        $('#modal-champion-tft').modal('show');
+    });
+
+    $(document).on('click', '.lm_xemthem_trangphuc', function (e) {
+        e.preventDefault();
+        $('#modal-lol-custom').modal('show');
+    });
+
     $(document).on('click', '.buyacc',function(e){
         e.preventDefault();
         var htmlloading = '';
