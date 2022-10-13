7ad1a743adf3a27d956479479b09c10f6cdc4982
iahman8101 <namdh@hqplay.vn>
2022-10-13 16:23:08 +0700

    update card theme 2

--- resources/views/theme_card_2/frontend/layouts/master.blade.php
+++ resources/views/theme_card_2/frontend/layouts/master.blade.php
@@ -253,136 +253,136 @@
         document.execCommand("copy");
         $temp.remove();
     }
-
-    $.ajax({
-        url: "/mua-the",
-        type: "GET",
-        success: function (data) {
-            let html = '';
-            // html += '<div id="flex-box">';
-            $.each(data.store_telecom, function (key, value) {
-                html += '<li>';
-                html += '<div class="nav-link link-supplier text-center" >';
-                html += '<input name="telecom_key" value="' + value['key'] + '" id="myCheckbox' + key + '" type="radio" />';
-                html += '<label class="item-wapper ' + value['key'] + '" for="myCheckbox' + key + '" onClick="reply_click(this.id)" id="' + value['key'] + '">';
-                if (value['image']) {
-                    html += "<img class=\"img-fluid\" src=\"" + value["image"] + "\">";
-                } else {
-                    html += value['title'];
-                }
-                // html += value['title'];
-                html += '</label></div>';
-                html += '</li>';
-            });
-            // html += '</div>';
-            $('#tab-supplier').html(html);
-        }
-    });
-
-    function formatNumber(num) {
-        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-    }
-
-
-    function reply_click(clicked_id) {
-        let html = '';
-        var getamount = $.get("/mua-the/get-amount?telecom_key=" + clicked_id, function (data, status) {
-            $.each(data.store_telecom_value, function (key, value) {
-                html += '<div class="text-center">';
-                html += '<input type="radio" name="amount" class="amount" id="CheckboxSupplier' + key + '" value="' + value['amount'] + '" rel-ratio="' + value['ratio_default'] + '"/>';
-                html += '<input style="display: none" type="text" id="price_' + value['amount'] + '" class="ratio_default" value="' + value['ratio_default'] + '"/>';
-                html += '<label class="item-content" for="CheckboxSupplier' + key + '">';
-                html += '<h5>' + formatNumber(value['amount']) + ' VNĐ </h5>';
-                html += '<p>Giá: <span>' + formatNumber(value['amount'] * value['ratio_default'] / 100) + ' VNĐ</span></p>';
-                html += '</label></div>';
-            });
-            $('#render-supplier').html(html);
-
-            let button = '';
-            button += '<div class="form-group m-form__group row ml-1">';
-            button += '<label class="col-lg-2 col-md-4 col-sm-4 col-4 col-form-label" style="font-weight:bold;font-size:16px;color:#2F6A7C">Số lượng: </label>';
-            button += '<div class="col-lg-2 col-md-4 col-sm-4 col-4">';
-            button += '<select class="form-control m-input m-input--air" name="quantity" id="quantity" style="border-color: #0000004f;">';
-            for (i = 1; i <= 10; i++) {
-                button += '<option class="quantity" value="' + i + '">' + i + '</option>';
-            }
-
-            button += '</select></div></div>';
-            $('#button').html(button);
-
-            var render_denominations = clicked_id;
-            $('.denominations').html(render_denominations);
-            $(".price_supplier").html("Chưa chọn");
-            $(".price_sale").html("0");
-            $(".render_quantity").html("1");
-            $(".total_price").html("0");
-        });
-
-        var top = $("#content").offset().top;
-        $([document.documentElement, document.body]).animate({
-            scrollTop: top
-        }, 500);
-
-    }
-
-    $("#recharge_supplier").on('submit', function (event) {
-        event.preventDefault();
-        var formData = new FormData(this);
-        $.ajax({
-            type: 'POST',
-            url: '/mua-the',
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            beforeSend: function () {
-                $(".content-ajax").hide();
-                $('.ajax-loader').show();
-            },
-            success: (data) => {
-                if (data.errors) {
-                    $('#modal_pay').modal('hide');
-                    swal(
-                        'Lỗi !',
-                        data.errors,
-                        'error'
-                    )
-                } else {
-                    $('#modal_pay').modal('hide');
-                    $('#showInfor').modal('show');
-                    let html = '';
-                    html += '<div class="id-infor" data-id="' + data.id + '"></div>';
-                    $('#showInforDetails').html(html);
-                    var id = $(".id-infor").data('id');
-                    $.ajax({
-                        url: "/mua-the/" + id + "/thong-tin-the",
-                        type: "GET",
-                        success: function (data) {
-                            // console.log(data.data.id);
-                            $("#id-item").html(id);
-                            $("#description-item").html(data.data.description);
-                            $("#money-item").html(formatNumber(data.data.price) + " VNĐ");
-                            $("#txns-item").html(100 - (data.data.txns[0].ratio) + " %");
-                            //
-                            // console.log(data.data.store_card);
-                            let html = '';
-                            $.each(data.arr, function (key, value) {
-                                html += '<tr>';
-                                html += '<td class="pin"><span>' + value['pin_item'] + '</span>&nbsp;<i style="cursor: pointer" class="fa fa-copy copyPin" aria-hidden="true"></i></td>';
-                                html += '<td class="serial"><span>' + value['serial_item'] + ' </span>&nbsp;<i style="cursor: pointer" class="fa fa-copy copySerial" aria-hidden="true"></i></td>';
-                                html += '</tr>';
-                            });
-                            $('#store_card').html(html);
-                        }
-                    });
-                }
-            },
-            complete: function () {
-                $('.ajax-loader').hide();
-                $(".content-ajax").show();
-            }
-        });
-    });
+    //
+    // $.ajax({
+    //     url: "/mua-the",
+    //     type: "GET",
+    //     success: function (data) {
+    //         let html = '';
+    //         // html += '<div id="flex-box">';
+    //         $.each(data.store_telecom, function (key, value) {
+    //             html += '<li>';
+    //             html += '<div class="nav-link link-supplier text-center" >';
+    //             html += '<input name="telecom_key" value="' + value['key'] + '" id="myCheckbox' + key + '" type="radio" />';
+    //             html += '<label class="item-wapper ' + value['key'] + '" for="myCheckbox' + key + '" onClick="reply_click(this.id)" id="' + value['key'] + '">';
+    //             if (value['image']) {
+    //                 html += "<img class=\"img-fluid\" src=\"" + value["image"] + "\">";
+    //             } else {
+    //                 html += value['title'];
+    //             }
+    //             // html += value['title'];
+    //             html += '</label></div>';
+    //             html += '</li>';
+    //         });
+    //         // html += '</div>';
+    //         $('#tab-supplier').html(html);
+    //     }
+    // });
+    //
+    // function formatNumber(num) {
+    //     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    // }
+    //
+    //
+    // function reply_click(clicked_id) {
+    //     let html = '';
+    //     var getamount = $.get("/mua-the/get-amount?telecom_key=" + clicked_id, function (data, status) {
+    //         $.each(data.store_telecom_value, function (key, value) {
+    //             html += '<div class="text-center">';
+    //             html += '<input type="radio" name="amount" class="amount" id="CheckboxSupplier' + key + '" value="' + value['amount'] + '" rel-ratio="' + value['ratio_default'] + '"/>';
+    //             html += '<input style="display: none" type="text" id="price_' + value['amount'] + '" class="ratio_default" value="' + value['ratio_default'] + '"/>';
+    //             html += '<label class="item-content" for="CheckboxSupplier' + key + '">';
+    //             html += '<h5>' + formatNumber(value['amount']) + ' VNĐ </h5>';
+    //             html += '<p>Giá: <span>' + formatNumber(value['amount'] * value['ratio_default'] / 100) + ' VNĐ</span></p>';
+    //             html += '</label></div>';
+    //         });
+    //         $('#render-supplier').html(html);
+    //
+    //         let button = '';
+    //         button += '<div class="form-group m-form__group row ml-1">';
+    //         button += '<label class="col-lg-2 col-md-4 col-sm-4 col-4 col-form-label" style="font-weight:bold;font-size:16px;color:#2F6A7C">Số lượng: </label>';
+    //         button += '<div class="col-lg-2 col-md-4 col-sm-4 col-4">';
+    //         button += '<select class="form-control m-input m-input--air" name="quantity" id="quantity" style="border-color: #0000004f;">';
+    //         for (i = 1; i <= 10; i++) {
+    //             button += '<option class="quantity" value="' + i + '">' + i + '</option>';
+    //         }
+    //
+    //         button += '</select></div></div>';
+    //         $('#button').html(button);
+    //
+    //         var render_denominations = clicked_id;
+    //         $('.denominations').html(render_denominations);
+    //         $(".price_supplier").html("Chưa chọn");
+    //         $(".price_sale").html("0");
+    //         $(".render_quantity").html("1");
+    //         $(".total_price").html("0");
+    //     });
+    //
+    //     var top = $("#content").offset().top;
+    //     $([document.documentElement, document.body]).animate({
+    //         scrollTop: top
+    //     }, 500);
+    //
+    // }
+    //
+    // $("#recharge_supplier").on('submit', function (event) {
+    //     event.preventDefault();
+    //     var formData = new FormData(this);
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: '/mua-the',
+    //         data: formData,
+    //         cache: false,
+    //         contentType: false,
+    //         processData: false,
+    //         beforeSend: function () {
+    //             $(".content-ajax").hide();
+    //             $('.ajax-loader').show();
+    //         },
+    //         success: (data) => {
+    //             if (data.errors) {
+    //                 $('#modal_pay').modal('hide');
+    //                 swal(
+    //                     'Lỗi !',
+    //                     data.errors,
+    //                     'error'
+    //                 )
+    //             } else {
+    //                 $('#modal_pay').modal('hide');
+    //                 $('#showInfor').modal('show');
+    //                 let html = '';
+    //                 html += '<div class="id-infor" data-id="' + data.id + '"></div>';
+    //                 $('#showInforDetails').html(html);
+    //                 var id = $(".id-infor").data('id');
+    //                 $.ajax({
+    //                     url: "/mua-the/" + id + "/thong-tin-the",
+    //                     type: "GET",
+    //                     success: function (data) {
+    //                         // console.log(data.data.id);
+    //                         $("#id-item").html(id);
+    //                         $("#description-item").html(data.data.description);
+    //                         $("#money-item").html(formatNumber(data.data.price) + " VNĐ");
+    //                         $("#txns-item").html(100 - (data.data.txns[0].ratio) + " %");
+    //                         //
+    //                         // console.log(data.data.store_card);
+    //                         let html = '';
+    //                         $.each(data.arr, function (key, value) {
+    //                             html += '<tr>';
+    //                             html += '<td class="pin"><span>' + value['pin_item'] + '</span>&nbsp;<i style="cursor: pointer" class="fa fa-copy copyPin" aria-hidden="true"></i></td>';
+    //                             html += '<td class="serial"><span>' + value['serial_item'] + ' </span>&nbsp;<i style="cursor: pointer" class="fa fa-copy copySerial" aria-hidden="true"></i></td>';
+    //                             html += '</tr>';
+    //                         });
+    //                         $('#store_card').html(html);
+    //                     }
+    //                 });
+    //             }
+    //         },
+    //         complete: function () {
+    //             $('.ajax-loader').hide();
+    //             $(".content-ajax").show();
+    //         }
+    //     });
+    // });
 
     $('#store_card').on('click', '.copyPin', function () {
         var text = $(this).prev().text()
@@ -505,6 +505,7 @@
 <script src="/assets/frontend/{{theme('')->theme_key}}/js/dashboard.js" type="text/javascript"></script>
 <script src="/assets/frontend/{{theme('')->theme_key}}/js/index.js" type="text/javascript"></script>
 <script src="/assets/frontend/{{theme('')->theme_key}}/js/main.js" type="text/javascript"></script>
+<script src="/assets/frontend/{{theme('')->theme_key}}/js/account_info.js" type="text/javascript"></script>
 <script src="/assets/frontend/{{theme('')->theme_key}}/lib/bootstrap-datetimepicker/bootstrap-datetimepicker.js" type="text/javascript"></script>
 <script src="/assets/frontend/{{theme('')->theme_key}}/lib/bootstrap-datetimepicker/bootstrap-datepicker.js" type="text/javascript"></script>
 </body>
