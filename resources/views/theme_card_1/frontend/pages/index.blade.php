3543ca042460b427974c9f91ca450a201578e59b
TienPhu0112 <phunt@hqplay.vn>
2022-11-08 18:50:56 +0700

    theme_card_1 watched acc and nick category list homepage

--- resources/views/theme_card_1/frontend/pages/index.blade.php
+++ resources/views/theme_card_1/frontend/pages/index.blade.php
@@ -28,6 +28,7 @@
             </div>
         </div>
         @include('frontend.widget.__content__home__dichvu')
+        @include('frontend.widget.__content__home__game')
         <div class="clr"></div>
         @if(setting('sys_intro_text'))
         <div class="wp_content_post_index">
