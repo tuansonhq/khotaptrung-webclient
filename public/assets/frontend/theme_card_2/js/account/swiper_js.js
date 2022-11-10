5d54bf6a010f06db22a969c67c71be70693ccac6
Duong36 <102272908+Duong36@users.noreply.github.com>
2022-11-10 20:40:11 +0700

    update

---
+++ public/assets/frontend/theme_card_2/js/account/swiper_js.js
@@ -0,0 +1,39 @@
+new Swiper('.swiper-account-related', {
+    autoplay: false,
+    updateOnImagesReady: true,
+    watchSlidesVisibility: false,
+    lazyLoadingInPrevNext: false,
+    lazyLoadingOnTransitionStart: false,
+    loop: false,
+    centeredSlides: false,
+    slidesPerView: 4,
+    speed: 800,
+    spaceBetween: 16,
+    freeMode: true,
+    touchMove: true,
+    freeModeSticky:true,
+    grabCursor: true,
+    observer: true,
+    observeParents: true,
+    keyboard: {
+        enabled: true,
+    },
+    breakpoints: {
+
+        992: {
+            slidesPerView: 4,
+        },
+        768: {
+            slidesPerView:3,
+        },
+
+        480: {
+            slidesPerView: 1.8,
+            spaceBetween: 6,
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-list-acc .swiper-list-next",
+        prevEl: ".swiper-list-acc .swiper-list-prev",
+    },
+});
