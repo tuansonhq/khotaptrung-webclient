3e6a41d473c72d816f586fa81864e1b66c9dba2d
TienPhu0112 <phunt@hqplay.vn>
2022-11-18 16:26:03 +0700

    theme_3 display auto ngoc rong acc data

--- resources/views/theme_1/frontend/pages/account/widget/__datalist.blade.php
+++ resources/views/theme_1/frontend/pages/account/widget/__datalist.blade.php
@@ -778,24 +778,6 @@
                                                         @if($total < 5)
                                                             @if($in->name == 'Skill Pet' || $in->name == 'Cải trang')
                                                                 @if($in->name == 'Skill Pet')
-{{--                                                                    <div class="row" style="margin: 0 auto;width: 100%">--}}
-{{--                                                                        <div class="col-auto text-left fixcssacount item_buy_list_info_inacc">--}}
-{{--                                                                            Skill đệ tử :--}}
-{{--                                                                        </div>--}}
-{{--                                                                        <div class="col-auto text-right fixcssacount item_buy_list_info_inaccright" style="color: #666;font-weight: 600;margin-left: auto">--}}
-{{--                                                                            Các cấp đang có--}}
-
-{{--                                                                            @if(isset($in->value) && count($in->value) )--}}
-{{--                                                                                @foreach($in->value as $k_value => $value)--}}
-
-{{--                                                                                    {{ $k_value + 1 }}--}}
-{{--                                                                                    @if($k_value + 1 < count($in->value))--}}
-{{--                                                                                        +--}}
-{{--                                                                                    @endif--}}
-{{--                                                                                @endforeach--}}
-{{--                                                                            @endif--}}
-{{--                                                                        </div>--}}
-{{--                                                                    </div>--}}
                                                                 @elseif($in->name == 'Cải trang')
                                                                     <?php
 
@@ -807,9 +789,6 @@
                                                                         </div>
                                                                         <div class="col-auto text-right fixcssacount item_buy_list_info_inaccright" style="color: #666;font-weight: 600;margin-left: auto">
                                                                             @if(isset($in->value) && count($in->value) )
-{{--                                                                                @foreach($in->value as $value)--}}
-{{--                                                                                    {{ $value }} <br>--}}
-{{--                                                                                @endforeach--}}
                                                                                 {{ count($in->value) }}
                                                                             @endif
                                                                         </div>
