fb8b8d86f640f65f5e1e6d0716cc8c97a56dd762
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-12-01 09:29:39 +0700

    Update
    Update

--- resources/views/theme_4/frontend/pages/index.blade.php
+++ resources/views/theme_4/frontend/pages/index.blade.php
@@ -37,19 +37,19 @@
 
                 @include('frontend.widget.__content__home__game')
 
-                <div class="d-flex justify-content-between" style="padding-top: 24px">
-                    <div class="main-title">
-                        <h1>Minigame</h1>
-                    </div>
-                    <div class="service-search d-none d-lg-block">
-                        <div class="input-group p-box">
-                            <input type="text" id="txtSearchMinigame" placeholder="Tìm minigame" value="" class="" width="200px">
-                            <span class="icon-search"><i class="fas fa-search"></i></span>
-                        </div>
-                    </div>
-                </div>
+{{--                <div class="d-flex justify-content-between" style="padding-top: 24px">--}}
+{{--                    <div class="main-title">--}}
+{{--                        <h1>Minigame</h1>--}}
+{{--                    </div>--}}
+{{--                    <div class="service-search d-none d-lg-block">--}}
+{{--                        <div class="input-group p-box">--}}
+{{--                            <input type="text" id="txtSearchMinigame" placeholder="Tìm minigame" value="" class="" width="200px">--}}
+{{--                            <span class="icon-search"><i class="fas fa-search"></i></span>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
-                @include('frontend.widget.__content__home__minigame')
+{{--                @include('frontend.widget.__content__home__minigame')--}}
 
             <div class="article-content content_post ">
                 <div class="special-text">
